9196de02129001d243779752d96c113615e24063
RWallace <ross.wallace@stratis-media.com>
2017-04-10 15:55:29 +1000

    I can has render?

--- application/config/routes.php
+++ application/config/routes.php
@@ -11,5 +11,5 @@ $route['vehicles/(:any)'] = 'vehicles/view/$1';
 $route['vehicles'] = 'vehicles/index';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
-$route['404_override'] = '';
+/*$route['404_override'] = '';*/
 $route['translate_uri_dashes'] = FALSE;
