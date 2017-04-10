73aeca24d5677c2f9d0134edf313718b5b47cdbc
paralittic <ross.wallace@stratis-media.com>
2017-04-10 20:55:40 +1000

    Add titles to vehicle pages

--- application/views/vehicles/create.php
+++ application/views/vehicles/create.php
@@ -1,7 +1,7 @@
 <head>
 	<title>Create Vehicle - Logi</title>
 </head>
-
+<h2 style="text-transform: uppercase;">Vehicles</h2>
 <?php echo validation_errors(); ?>
 <?php $attributes = array('class' => 'form-horizontal'); ?>
 <?php echo form_open('vehicles/create', $attributes); ?>
