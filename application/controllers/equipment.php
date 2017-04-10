97e73c65b5d9691d6bb6381a6a2ad12468fc946a
RWallace <ross.wallace@stratis-media.com>
2017-04-10 14:52:01 +1000

    caps...are you serious

--- application/controllers/equipment.php
+++ application/controllers/equipment.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-        class equipment extends CI_Controller {
+        class Equipment extends CI_Controller {
                 public function index(){
                         $data['title'] = 'Equipment'; 
 
--- application/controllers/vehicles.php
+++ application/controllers/vehicles.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-        class vehicles extends CI_Controller {
+        class Vehicles extends CI_Controller {
                 public function index(){
                         $data['title'] = 'Vehicles'; 
 
