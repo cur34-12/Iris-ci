7a7096d0958863887d73a108b17ffcf8cb75fa13
paralittic <ross.wallace@stratis-media.com>
2017-04-11 16:43:41 +1000

    Removed link from table

--- application/views/vehicles/view.php
+++ application/views/vehicles/view.php
@@ -3,7 +3,7 @@
 </head>
 <h2><?php echo strtoupper($vehicle['name']); ?></h2>
 <table class="table table-striped table-hover ">
-	<tr><td style="font-weight: bold">Name</td><td style="text-transform: uppercase;"><a href="<?php echo site_url('/vehicles/'. $vehicle['name']); ?>"><?php echo $vehicle['name']; ?></a></td></tr>
+	<tr><td style="font-weight: bold">Name</td><td style="text-transform: uppercase;"><?php echo strtoupper($vehicle['name']); ?></td></tr>
 	<tr><td style="font-weight: bold">Rego</td><td><?php echo $vehicle['rego']; ?></td></tr>
 	<tr><td style="font-weight: bold">Capacity</td><td><?php echo $vehicle['capacity']; ?></td></tr>
 	<tr><td style="font-weight: bold">Category</td><td><?php echo $vehicle['category']; ?></td></tr>
@@ -22,8 +22,6 @@
 <a class="btn btn-info" role="button" href="<?php echo site_url('/vehicles/edit/'. $vehicle['name']); ?>">Edit Vehicle</a>
 
 <!--Delete Button (as form to protect against abuse)-->
-
-
 <?php $attributes = array('class' => 'form-inline', 'style' => 'display: inline;'); ?>
 <?php echo form_open('/vehicles/delete/'.$vehicle['name'], $attributes); ?>
 	<input type="submit" value="Delete Vehicle" class="btn btn-danger"/>
