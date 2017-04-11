48cfcfd49c60dd4ff1843a4cf2e186a7f36bf399
paralittic <ross.wallace@stratis-media.com>
2017-04-11 16:05:50 +1000

    Adding further features to vehicle views

--- application/views/vehicles/view.php
+++ application/views/vehicles/view.php
@@ -1,13 +1,23 @@
 <head>
 	<title><?php echo strtoupper($vehicle['name']); ?> - Logi</title>
 </head>
-<h2><?php echo $vehicle['make']; ?></h2>
-
+<table>
+	<tr><td style="text-transform: uppercase;"><a href="<?php echo site_url('/vehicles/'. $vehicle['name']); ?>"><?php echo $vehicle['name']; ?></a></td></tr>
+	<tr><td><?php echo $vehicle['rego']; ?></td></tr>
+	<tr><td><?php echo $vehicle['capacity']; ?></td></tr>
+	<tr><td><?php echo $vehicle['category']; ?></td></tr>
+	<tr><td><?php echo $vehicle['lr']; ?></td></tr>
+	<tr><td><?php echo $vehicle['make']; ?></td></tr>
+	<tr><td><?php echo $vehicle['year']; ?></td></tr>
+	<tr><td><?php echo $vehicle['model']; ?></td></tr>
+</table>
 <hr />
 <a class="btn btn-default" role="button" href="<?php echo site_url('/vehicles/create'); ?>">Create Vehicle</a>
 <a class="btn btn-info" role="button" href="<?php echo site_url('/vehicles/edit/'. $vehicle['name']); ?>">Edit Vehicle</a>
-<?php $attributes = array('class' => 'form-inline', 'style' => 'display: inline;'); ?>
+<?php $attributes = array('class' => 'form-inline', 'style' => 'display: inline;'), 'onsubmit' => 'return confirm('Do you really want to delete <?php echo strtoupper($vehicle['name']); ?>'); ?>
 <?php echo form_open('/vehicles/delete/'.$vehicle['name'], $attributes); ?>
+	<form onsubmit="return confirm('Do you really want to delete <?php echo strtoupper($vehicle['name']); ?>?');">
 	<input type="submit" value="Delete Vehicle" class="btn btn-danger"/>
 </form>
 
+confirm("Press a button!");
