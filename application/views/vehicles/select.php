5ac1dd11aa25dbb95b536252c4615b1ce6841115
RWallace <ross.wallace@stratis-media.com>
2017-04-12 11:16:54 +1000

    Fixed urls on the table

--- application/views/vehicles/select.php
+++ application/views/vehicles/select.php
@@ -4,10 +4,13 @@
 <h2 style="text-transform: uppercase;">Edit - Select a Vehicle</h2>
 <?php $attributes = array('class' => 'form-horizontal'); ?>
 
-<table>
+<table class="table table-striped table-hover ">
+<thead>
+<td>Vehicle</td>
+</thead>
 <?php foreach($vehicles as $vehicle) : ?>
 <tr>
-<td style="text-transform: uppercase;"><a href="<?php echo site_url('/vehicles/edit'. $vehicle['name']); ?>"><?php echo $vehicle['name']; ?></a></td>
+<td style="text-transform: uppercase;"><a href="<?php echo site_url('/vehicles/edit/'. $vehicle['name']); ?>"><?php echo $vehicle['name']; ?></a></td>
 </tr>
 <?php endforeach; ?>
 </table>
