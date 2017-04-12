6bf0840edbc2078fba68950a809871ed10c04d0d
Ross Wallace <ross.wallace@stratis-media.com>
2017-04-13 00:07:03 +1000

    CSS bootswatch switch

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
         <head>
 
 				<!-- Latest compiled and minified bootswatch CSS, in the future it would be cool to have the user be able to select between day and night mode from their settings or something -->
-				<!--night mode--><link rel="stylesheet" href="https://bootswatch.com/slate/bootstrap.min.css">
+				<!--night mode--><link rel="stylesheet" href="https://bootswatch.com/solar/bootstrap.min.css">
                                 <!--day mode--><!--<link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">-->
 
                                 <!-- Custom CSS -->
