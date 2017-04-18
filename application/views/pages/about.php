b8c8b4f51fdf1a9feba10334befd9c641ea44a83
paralittic <ross.wallace@stratis-media.com>
2017-04-18 17:54:59 +1000

    Partially converted markdown to html

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,37 +1,41 @@
 <head>
 	<title>About - Logi</title>
 </head>
-<xmp theme="united" style="display:none;">
-# Logi - An emergency services resource management system
-Developed with unit based NSW State Emergency Service users in mind Logi aims to create a multifaceted data management system. The main functions sit around the marrying of users/members to equipment loans and time management. The system is build completely with user experience in mind so that minimal training or guidance is required for use from both a manager and end user perspective.
-
-## Functional Areas
-### Sign-on/Time Management
+<h2>Logi - An emergency services resource management system</h2>
+<p>Developed with unit based NSW State Emergency Service users in mind Logi aims to create a multifaceted data management system. The main functions sit around the marrying of users/members to equipment loans and time management. The system is build completely with user experience in mind so that minimal training or guidance is required for use from both a manager and end user perspective.</p>
+<h3>Functional Areas</h3>
+<h4>Sign-on/Time Management</h4>
 Track and manage the time onsite and time type of members. Facilitates the reporting of activity and attendance and automated reporting.
-#### Planned
-- Member clock on/clock off
-- Visitor clock on/clock Off
-- Visit/time categorisation
-- Currently onsite list
-- Reporting/scheduled email reporting
-- Barcode scanning/ID based sign-on
-- Data export (xls/csv)
-- Dashboard for operations screens to view members onsite and rank/skill level
-#### In Progress
-- None yet :(
-#### Completed
-- None yet :(
+<h5>Planned</h5>
+<ul>
+<li>Member clock on/clock off</li>
+<li>Visitor clock on/clock Off</li>
+<li>Visit/time categorisation</li>
+<li>Currently onsite list</li>
+<li>Reporting/scheduled email reporting</li>
+<li>Barcode scanning/ID based sign-on</li>
+<li>Data export (xls/csv)</li>
+<li>Dashboard for operations screens to view members onsite and rank/skill level</li>
+</ul>
+<h5>In Progress</h5>
+<ul>
+<li>None yet</li>
+</ul>
+<h5>Completed</h5>
+<li>None yet :(</li>
 
-### Inventory
-Track and manage the movement, allocation and level of inventory held in the unit through the use of barcoding. 
-#### Planned
-- Team/Manager responsible
-- Reporting and export
-- Automatic notifications of stock reductions
-- Inspection management and scheduling
-- Logging of loans of specific equipment
-- Email notifications if out over 24 hours
-- List of current and historical loans with which member
+<h4>Inventory</h4>
+<p>Track and manage the movement, allocation and level of inventory held in the unit through the use of barcoding. </p>
+<h5>Planned</h5>
+<ul>
+<li>Team/Manager responsible</li>
+<li>Reporting and export</li>
+<li>Automatic notifications of stock reductions</li>
+<li>Inspection management and scheduling</li>
+<li>Logging of loans of specific equipment</li>
+<li>Email notifications if out over 24 hours</li>
+<li>List of current and historical loans with which member</li>
+</ul>
 #### In Progress
 - Equipment quantity tracking
 - Barcode management of assets
