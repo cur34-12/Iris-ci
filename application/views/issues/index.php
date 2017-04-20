cfa3aa23681a8d0d3c022189b6906e000be67400
paralittic <ross.wallace@stratis-media.com>
2017-04-20 16:11:27 +1000

    maybe was doing this a stupid way

--- application/views/issues/index.php
+++ application/views/issues/index.php
@@ -18,7 +18,7 @@
 		</tr>
 	</thead>
 	<!--Fields must be added to this table if an extra field is added to the relevant table in the database-->
-	<tbody><?php $i = 0; ?>
+	<tbody>
 		<?php foreach($issues as $issue) : ?>
 			<tr>
 					<td><a href="<?php echo site_url('/issues/'. $issue['issueID']); ?>"><?php echo $issue['issueID']; ?></a></td>
@@ -26,37 +26,14 @@
 					<td><?php echo $issue['title']; ?></td>
 					<td><?php echo $issue['reporterID']; ?></td>
 					<td><?php echo $issue['assigned_user']; ?></td>
-					<?php echo "<td id=\"reported_date_$i\"></td>"; ?>
-					<?php echo "<td id=\"resolution_date_$i\"></td>"; ?>
+					<?php echo "<td><script>moment('<?php echo $issue['reported_date'] ?>').format('DD-MM-YYYY');</script></td>"; ?>
+					<?php echo "<td><script>moment('<?php echo $issue['resolution_date'] ?>').format('DD-MM-YYYY');</script></td>"; ?>
 					<td><?php echo $issue['status']; ?></td>
 					<td><a class="btn btn-primary btn-sm" role="button" href="issues/edit/<?php echo $issue['issueID']; ?>">Edit</a></td>
 					
-					<script type="text/javascript">
-						var reported_if;
-						$reported_if = <?php $issue['reported_date'] ?>;
-						console.log($reported_if);
-						if ($reported_if == "0000-00-00") {
-							$('#reported_date_<?php echo $i ?>').html("&nbsp");
-						} else {
-							var parsed_reported_date_<?php echo $i ?> = moment('<?php echo $issue['reported_date'] ?>').format('DD-MM-YYYY'); 
-							$('#reported_date_<?php echo $i ?>').html(parsed_reported_date_<?php echo $i ?>);
-						}
-			    	</script>
-
-					<script type="text/javascript">
-						var resolved_if; 
-						$resolved_if = <?php $issue['resolution_date'] ?>;
-						console.log($resolved_if);
-						if ($resolved_if == "0000-00-00") {
-							$('#resolution_date_<?php echo $i ?>').html("&nbsp");
-						} else {
-							var parsed_resolution_date_<?php echo $i ?> = moment('<?php echo $issue['resolution_date'] ?>').format('DD-MM-YYYY'); 
-							$('#resolution_date_<?php echo $i ?>').html(parsed_resolution_date_<?php echo $i ?>);
-						}
-			    	</script>
+					
 
 			</tr>
-			<?php $i++; ?>
 		<?php endforeach; ?>
 	</tbody>
 </table>
