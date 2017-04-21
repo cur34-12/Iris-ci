2029af4296fbcf0bf1da0f49bf12f69732287c0d
paralittic <ross.wallace@stratis-media.com>
2017-04-21 14:26:57 +1000

    Updated session to be database driven

--- application/config/config.php
+++ application/config/config.php
@@ -183,7 +183,7 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 |
 */
 $config['allow_get_array'] = TRUE;
-$config['enable_query_strings'] = FALSE;
+$config['enable_query_strings'] = TRUE;
 $config['controller_trigger'] = 'c';
 $config['function_trigger'] = 'm';
 $config['directory_trigger'] = 'd';
@@ -378,10 +378,10 @@ $config['encryption_key'] = 'wdqCZVUsXkx3xLhHnHzS9NPgIQn3O620';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = '/var/www/logi/sessions/';
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
