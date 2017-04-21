0d154130d4f07296c3f1552c5da72d66fac7ef29
paralittic <ross.wallace@stratis-media.com>
2017-04-21 16:33:59 +1000

    removed the serialize helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form','cookie','serialize');
+$autoload['helper'] = array('url', 'form','cookie');
 
 /*
 | -------------------------------------------------------------------
