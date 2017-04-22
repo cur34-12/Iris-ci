c0229a6b8de4b9eda27fe39382530ca92b139670
paralittic <ross.wallace@stratis-media.com>
2017-04-22 11:25:55 +1000

    Removed tokens

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('form_validation','database','session','zend','tokens');
+$autoload['libraries'] = array('form_validation','database','session','zend');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form','cookie');
+$autoload['helper'] = array('url','form','cookie');
 
 /*
 | -------------------------------------------------------------------
