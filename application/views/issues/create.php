04bccb80538af57c92654a29d91080eedf93fb50
paralittic <ross.wallace@stratis-media.com>
2017-04-22 14:58:39 +1000

    Updated array handler

--- application/views/issues/create.php
+++ application/views/issues/create.php
@@ -54,8 +54,8 @@
 		<div class="col-sm-10">
 			<select id="assigned_user" name="assigned_user" class="form-control" >
 				<?php
-        			foreach($usernames as $username) {
-        				echo "<option>".$user_list->username."</option>";
+        			foreach($username as $usernames) {
+        				echo "<option>".echo $username['username']."</option>";
         			}
         		?>
 			</select>
