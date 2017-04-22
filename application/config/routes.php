42ecb95d95ca7ebf7a0749a39b4769c00dd07177
paralittic <ross.wallace@stratis-media.com>
2017-04-22 11:29:09 +1000

    More removal of CA

--- application/config/routes.php
+++ application/config/routes.php
@@ -42,7 +42,6 @@ $route['vehicles/(:any)'] = 'vehicles/view/$1';
 $route['vehicles'] = 'vehicles/index';
 
 /* Catch All Routes */
-$route[LOGIN_PAGE] = 'examples/login';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
