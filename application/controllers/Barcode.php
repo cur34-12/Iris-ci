5d2fd38b011084737315812303891b5648eb2919
paralittic <ross.wallace@stratis-media.com>
2017-04-23 22:03:20 +1000

    Updated function name

--- application/controllers/Barcode.php
+++ application/controllers/Barcode.php
@@ -1,5 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    class Main extends MY_Controller {
+    
+    class Barcode extends MY_Controller {
 
         public function index()
         {
