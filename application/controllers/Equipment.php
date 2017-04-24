c549126ec266b73abede148f6fc6ad72fb5e9727
paralittic <ross.wallace@stratis-media.com>
2017-04-24 20:38:38 +1000

    commented out zend

--- application/controllers/Equipment.php
+++ application/controllers/Equipment.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
         
-        use Zend\Barcode\Barcode;
+        //use Zend\Barcode\Barcode;
 
         class Equipment extends MY_Controller {
                 public function index(){
