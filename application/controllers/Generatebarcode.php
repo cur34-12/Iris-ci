65860f7c27343a13fc46765b571f6c444f8b5c82
paralittic <ross.wallace@stratis-media.com>
2017-04-24 12:05:38 +1000

    More barcode testing

--- application/controllers/Generatebarcode.php
+++ application/controllers/Generatebarcode.php
@@ -2,14 +2,23 @@
         
     use Zend\Barcode\Barcode;
 
-   public function generatebarcode(){
-       $this->set_barcode("12345");
-   }
+	class Generatebarcode extends MY_Controller {
 
-   private function set_barcode($code)
-   {
-
-        $file = Barcode::draw('code128', 'image', array('text' => $code), array());
-
-        $store_image = imagepng($file,FCPATH."assets/barcode/{$code}.png");
-   }
+		public function index()
+		{
+			//I'm just using rand() function for data example
+			$temp = rand(10000, 99999);
+			$this->set_barcode($temp);
+		}
+		
+		private function set_barcode($code)
+		{
+			//load library
+			$this->load->library('zend');
+			//load in folder Zend
+			$this->zend->load('Zend/Barcode');
+			//generate barcode
+			Zend_Barcode::render('code128', 'image', array('text'=>$code), array());
+		}
+		
+	}
