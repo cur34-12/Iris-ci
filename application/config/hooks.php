415415051698b1b8e340267d19357de5c29a1e61
paralittic <ross.wallace@stratis-media.com>
2017-04-28 16:30:25 +1000

    Removed duplication

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -21,13 +21,3 @@ $hook['post_system'] = array(
     'filename' => 'auth_sess_check.php',
     'filepath' => 'hooks'
 );
-$hook['pre_system'] = array(
-    'function' => 'auth_constants',
-    'filename' => 'auth_constants.php',
-    'filepath' => 'hooks'
-);
-$hook['post_system'] = array(
-    'function' => 'auth_sess_check',
-    'filename' => 'auth_sess_check.php',
-    'filepath' => 'hooks'
-);
