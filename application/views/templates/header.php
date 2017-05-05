0df5b20f3d28d379a90e661df15a708b85e171d1
paralittic <ross.wallace@stratis-media.com>
2017-05-05 15:28:52 +1000

    Removed header variables

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,11 +2,8 @@
         <head>
 
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-            <meta http-equiv="content-language" content="<?php echo $lang ?>">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta name="keywords" content="<?php echo  $meta_keywords ?>">
-            <meta name="description" content="<?php echo  $meta_description ?>">
+
 
             <!-- Jquery -->
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
