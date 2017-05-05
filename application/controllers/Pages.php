d128e448b554a24a6b3c86defe4e890a6727fc28
paralittic <ross.wallace@stratis-media.com>
2017-05-05 15:25:59 +1000

    Header fixes

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -8,7 +8,18 @@
                         show_404();
         	       }
 
-        	       $data['title'] = ucfirst($page); // Capitalize the first letter
+                        $data['title'] = ucfirst($page); // Capitalize the first letter
+                        $data['lang'] = $this->setting['site_language'];                 
+                
+                        $this->languages->get_lang($data['lang']);
+                
+                        $data['site_name'] = $this->setting['site_name'];
+                        $data['meta_keywords'] = $this->setting['meta_keywords'];
+                        $data['meta_description'] = $this->setting['meta_description'];  
+                        $data['key'] = $this->setting['cal_apikey'];
+                        $data['current_version'] = $this->setting['current_version'];  
+
+                        $data['timezone'] = $this->setting['site_timezone']; 
 
         	       $this->load->view('templates/header', $data);
         	       $this->load->view('pages/'.$page, $data);
