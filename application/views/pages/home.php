d83b1d2e74649c603f6116911e5086b00ecb6510
paralittic <ross.wallace@stratis-media.com>
2017-05-12 09:56:31 +1000

    Adding typeahead

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,3 +2,31 @@
 	<title>Logi</title>
 </head>
 <p>Welcome to Logi, an SES focused logistics resource planning system</p>
+
+
+<style>
+#custom-templates .empty-message {
+  padding: 5px 10px;
+ text-align: center;
+}
+</style>
+
+<div id="custom-templates">
+  <input class="typeahead" type="text" placeholder="Oscar winners for Best Picture">
+</div>
+
+<script>
+$('#custom-templates .typeahead').typeahead(null, {
+  name: 'best-pictures',
+  display: 'value',
+  source: bestPictures,
+  templates: {
+    empty: [
+      '<div class="empty-message">',
+        'unable to find any Best Picture winners that match the current query',
+      '</div>'
+    ].join('\n'),
+    suggestion: Handlebars.compile('<div><strong>{{value}}</strong> – {{year}}</div>')
+  }
+});
+</script>
