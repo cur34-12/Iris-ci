41d2d882385703bda5156a8344ff6dfd5319b79b
paralittic <ross.wallace@stratis-media.com>
2017-05-12 11:43:29 +1000

    No brackets one line

--- search-equipment.php
+++ search-equipment.php
@@ -7,7 +7,7 @@
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 	$sql = "SELECT equipment.eq_id, equipment.eq_name, equipment.eq_asset_number, eqipment.eq_serial FROM equipment 
-			WHERE (eq_name LIKE '%".$_GET['query']."%' OR eq_asset LIKE '%".$_GET['query']."%' OR eq_serial LIKE '%".$_GET['query']."%')
+			WHERE eq_name LIKE '%".$_GET['query']."%' OR eq_asset LIKE '%".$_GET['query']."%' OR eq_serial LIKE '%".$_GET['query']."%'
 			LIMIT 10"; 
 	$result = $mysqli->query($sql);
 	
