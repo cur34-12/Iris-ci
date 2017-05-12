c3ee9a223133181481f86fc8eec0c5a36bba1e89
paralittic <ross.wallace@stratis-media.com>
2017-05-12 11:31:16 +1000

    Adding more json arral elements

--- search-equipment.php
+++ search-equipment.php
@@ -6,14 +6,19 @@
 	define (DB_HOST, "int.db1.strat.is");
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
-	$sql = "SELECT equipment.eq_name FROM equipment 
-			WHERE eq_name LIKE '%".$_GET['query']."%'
+	$sql = "SELECT equipment.eq_id, equipment.eq_name, equipment.eq_asset_number, eqipment.eq_serial FROM equipment 
+			WHERE eq_name LIKE '%".$_GET['query']."%' 
+			OR eq_asset LIKE '%".$_GET['query']."%' 
+			OR eq_serial LIKE '%".$_GET['query']."%' 
 			LIMIT 10"; 
 	$result = $mysqli->query($sql);
 	
 	$json = [];
 	while($row = $result->fetch_assoc()){
-	     $json[] = $row['eq_name'];
+	     $json['id'] = $row['eq_id'];
+	     $json['name'] = $row['eq_name'];
+	     $json['asset'] = $row['eq_asset_number'];
+	     $json['serial'] = $row['eq_serial'];	     
 	}
 
 	echo json_encode($json);
