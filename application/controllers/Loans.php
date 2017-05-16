04c739c81ab2748f3dfd8c90d497f744b2f52ad1
paralittic <ross.wallace@stratis-media.com>
2017-05-16 10:59:01 +1000

    Create loans

---
+++ application/controllers/Loans.php
@@ -0,0 +1,80 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+        
+        //use Zend\Barcode\Barcode;
+
+        class Equipment extends CI_Controller {
+                public function index(){
+                    $data['title'] = 'Loans'; 
+
+                    $data['loans'] = $this->Loans_model->get_loans();
+                        
+        	       $this->load->view('templates/header', $data);
+        	       $this->load->view('loans/index', $data);
+        	       $this->load->view('templates/footer');
+                }
+
+                public function view($loan_id = null){
+                        $data['loans'] = $this->Loans_model->get_loans($loan_id);
+
+                        if(empty($data['loans'])){
+                                show_404();
+                        } 
+
+                        $data['title'] = $data['loans']['loan_id'];
+
+                        $this->load->view('templates/header', $data);
+                        $this->load->view('loans/view', $data);
+                        $this->load->view('templates/footer');
+                }
+
+                public function create(){
+                        $data['title'] = 'Create Loan';
+
+                        $this->load->view('templates/header', $data);
+                        $this->load->view('loans/create', $data);
+                        $this->load->view('templates/footer');
+                }
+
+                public function delete($loan_id){
+                        $this->Loans_model->delete_loan($loan_id);
+                        redirect('loans');
+                }
+
+                public function edit($loan_id){
+                        $data['loans'] = $this->Loans_model->get_loans($loan_id);
+
+                        if(empty($data['loans'])){
+                                show_404();
+                        }
+
+                        $data['title'] = 'Edit $loan_id';
+
+                        $this->load->view('templates/header', $data);
+                        $this->load->view('equipment/update', $data);
+                        $this->load->view('templates/footer');
+                }
+
+                public function update(){
+                        $this->Loans_model->update_loan();
+                        redirect('loans');
+                }
+
+                public function select_edit(){ 
+
+                    $data['loans'] = $this->Loans_model->get_loans();
+                        
+                    $this->load->view('templates/header', $data);
+                    $this->load->view('equipment/edit', $data);
+                    $this->load->view('templates/footer');
+                }
+                
+                public function select_delete(){ 
+
+                    $data['loans'] = $this->Loans_model->get_loans();
+                        
+                    $this->load->view('templates/header', $data);
+                    $this->load->view('loans/delete', $data);
+                    $this->load->view('templates/footer');
+                }
+
+        }
---
+++ application/models/Loans_model.php
@@ -0,0 +1,67 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+	class Loans_model extends CI_Model{
+		public function __construct(){
+			$this->load->database();
+		}
+
+		public function get_loans($loan_id = FALSE){
+			if($loan_id === FALSE){
+				$this->db->order_by('loan_out_date', 'ASC');
+				$this->db->join('loan_groups', 'loan_groups.loangroup_id = loans.loan_group');
+				$this->db->join('members', 'members.member_id = loans.loan_member');
+				$this->db->join('equipment', 'equipment.member_id = loans.loan_equipment');
+				$this->db->join('users', 'users.user_id = loans.loan_user');
+				$query = $this->db->get('loans');
+				return $query->result_array();
+			}
+			$this->db->join('loan_groups', 'loan_groups.loangroup_id = loans.loan_group');
+			$this->db->join('members', 'members.member_id = loans.loan_member');
+			$this->db->join('equipment', 'equipment.member_id = loans.loan_equipment');
+			$this->db->join('users', 'users.user_id = loans.loan_user');
+			$query = $this->db->get_where('loans', array('loan_id' => $loan_id));
+			return $query->row_array();
+		}
+
+		public function create_loan(){
+			$data = array(
+				'loan_id' => $this->input->post('loan_id'),
+				'loan_member' => $this->input->post('loan_member'),
+				'loan_equipment' => $this->input->post('loan_equipment'),
+				'loan_out_date' => $this->input->post('loan_out_date'),
+				'loan_in_date' => $this->input->post('loan_in_date'),
+				'loan_user' => $this->input->post('loan_user'),
+				'loan_last_modified' => $this->input->post('loan_last_modified'),
+				'loan_last_modified_user' => $this->input->post('loan_last_modified_user'),
+				'loan_created_date' => $this->input->post('loan_created_date'),
+				'loan_group' => $this->input->post('loan_group'),
+				'loan_comment' => $this->input->post('loan_comment')
+			);
+
+			return $this->db->insert('loans', $data);
+		}
+
+		public function delete_loan($loan_id){
+			$this->db->where('loan_id', $loan_id);
+			$this->db->delete('loans');
+			return true;
+		}
+
+		public function update_loan(){
+			$data = array(
+				'loan_id' => $this->input->post('loan_id'),
+				'loan_member' => $this->input->post('loan_member'),
+				'loan_equipment' => $this->input->post('loan_equipment'),
+				'loan_out_date' => $this->input->post('loan_out_date'),
+				'loan_in_date' => $this->input->post('loan_in_date'),
+				'loan_user' => $this->input->post('loan_user'),
+				'loan_last_modified' => $this->input->post('loan_last_modified'),
+				'loan_last_modified_user' => $this->input->post('loan_last_modified_user'),
+				'loan_group' => $this->input->post('loan_group'),
+				'loan_comment' => $this->input->post('loan_comment')
+			);
+
+			$this->db->where('loan_id', $this->input->post('loan_id'));
+			return $this->db->update('loans', $data);
+		}
+	}
