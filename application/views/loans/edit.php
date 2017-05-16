9cf8cec4c04f5aa1bb2161ce10bb5fe40411a00c
paralittic <ross.wallace@stratis-media.com>
2017-05-16 22:42:10 +1000

    Cleaning up table

--- application/views/loans/edit.php
+++ application/views/loans/edit.php
@@ -6,12 +6,14 @@
 
 <table class="table table-striped table-hover ">
 <thead>
-<td>Loans</td>
+<th>Loan ID</th>
+<th>Member</th>
+<th>Issued Date</th>
 </thead>
 <?php foreach($loans as $loan) : ?>
 <tr>
 <td style="text-transform: uppercase;"><a href="<?php echo site_url('/loans/edit/'. $loan['loan_id']); ?>"><?php echo $loan['loan_id']; ?></a></td>
-<td style="text-transform: uppercase;"><a href="<?php echo site_url('/loans/edit/'. $loan['loan_id']); ?>"><?php echo $loan['loan_member']; ?></a></td>
+<td style="text-transform: uppercase;"><a href="<?php echo site_url('/loans/edit/'. $loan['loan_id']); ?>"><?php echo $loan['member_name']; ?></a></td>
 <td style="text-transform: uppercase;"><a href="<?php echo site_url('/loans/edit/'. $loan['loan_id']); ?>"><?php echo $loan['loan_out_date']; ?></a></td>
 </tr>
 <?php endforeach; ?>
