3935dba2c9be3c3e321201e08e9834cda6dfa030
paralittic <ross.wallace@stratis-media.com>
2017-05-18 10:06:07 +1000

    Fix model name

--- application/models/Unitmembers_model.php
+++ application/models/Unitmembers_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 	
-	class members_model extends CI_Model{
+	class Unitmembers_model extends CI_Model{
 		public function __construct(){
 			$this->load->database();
 		}
