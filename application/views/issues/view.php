ab1f5eb2ea8ddc93a238836c58aafcf6247a4ae4
paralittic <ross.wallace@stratis-media.com>
2017-05-18 11:17:19 +1000

    Inconcistancy with DB fixes

--- application/views/issues/view.php
+++ application/views/issues/view.php
@@ -11,7 +11,7 @@
 	<tr><td style="font-weight: bold">Assigned User</td><td><?php echo $issue['iss_assigned_user']; ?></td></tr>
 	<tr><td style="font-weight: bold">Reported Date</td><?php echo '<td style="font-weight: bold"><script type="text/javascript">var rp_date; $rp_date = moment(\''.$issue['iss_reported_date'].'\').format(\'DD-MM-YYYY\'); document.write($rp_date);</script></td>'; ?></tr>
 	<tr><td style="font-weight: bold">Resolution Date</td><?php echo '<td style="font-weight: bold"><script type="text/javascript">var res_date; $res_date = moment(\''.$issue['iss_resolution_date'].'\').format(\'DD-MM-YYYY\'); document.write($res_date);</script></td>'; ?></tr>
-	<tr><td style="font-weight: bold">Created By</td><td><?php echo $issue['iss_creator_idl']; ?></td></tr>
+	<tr><td style="font-weight: bold">Created By</td><td><?php echo $issue['iss_creator_id']; ?></td></tr>
 </table>
 <hr />
 
@@ -20,10 +20,10 @@
 <a class="btn btn-default" role="button" href="<?php echo site_url('/issues/create'); ?>">Create Issue</a>
 
 <!--Edit Button-->
-<a class="btn btn-info" role="button" href="<?php echo site_url('/vissues/edit/'. $issue['iss_title']); ?>">Edit Issue</a>
+<a class="btn btn-info" role="button" href="<?php echo site_url('/vissues/edit/'. $issue['iss_id']); ?>">Edit Issue</a>
 
 <!--Delete Button (as form to protect against abuse)-->
 <?php $attributes = array('class' => 'form-inline', 'style' => 'display: inline;'); ?>
-<?php echo form_open('/issues/delete/'.$issue['iss_title'], $attributes); ?>
+<?php echo form_open('/issues/delete/'.$issue['iss_id'], $attributes); ?>
 	<input type="submit" value="Delete Issue" class="btn btn-danger"/>
 </form>
