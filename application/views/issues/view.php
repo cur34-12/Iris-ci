fac070545318b8df554a928699771903d8e85b7c
paralittic <ross.wallace@stratis-media.com>
2017-05-18 11:15:13 +1000

    Typo

--- application/views/issues/view.php
+++ application/views/issues/view.php
@@ -7,7 +7,7 @@
 	<tr><td style="font-weight: bold">Equipment</td><td><a href="/equipment/view/<?php echo['eq_id']; ?>"><?php echo $issue['eq_name']; ?></a></td></tr>
 	<tr><td style="font-weight: bold">Status</td><td><?php echo $issue['iss_status']; ?></td></tr>
 	<tr><td style="font-weight: bold">Description</td><td><?php echo $issue['iss_description']; ?></td></tr>
-	<tr><td style="font-weight: bold">Reported By</td><td><?php echo $issue['iss_reported_id']; ?></td></tr>
+	<tr><td style="font-weight: bold">Reported By</td><td><?php echo $issue['iss_reporter_id']; ?></td></tr>
 	<tr><td style="font-weight: bold">Assigned User</td><td><?php echo $issue['iss_assigned_user']; ?></td></tr>
 	<tr><td style="font-weight: bold">Reported Date</td><?php echo '<td style="font-weight: bold"><script type="text/javascript">var rp_date; $rp_date = moment(\''.$issue['iss_reported_date'].'\').format(\'DD-MM-YYYY\'); document.write($rp_date);</script></td>'; ?></tr>
 	<tr><td style="font-weight: bold">Resolution Date</td><?php echo '<td style="font-weight: bold"><script type="text/javascript">var res_date; $res_date = moment(\''.$issue['iss_resolution_date'].'\').format(\'DD-MM-YYYY\'); document.write($res_date);</script></td>'; ?></tr>
