7d0e580e9b749b961961931ce94a7993bdc0b4b1
paralittic <ross.wallace@stratis-media.com>
2017-05-18 11:14:24 +1000

    Updated view to match db

--- application/views/issues/view.php
+++ application/views/issues/view.php
@@ -4,10 +4,10 @@
 <h2><?php echo $issue['iss_title']; ?></h2>
 <table class="table table-striped table-hover ">
 	<tr><td style="font-weight: bold">Title</td><td><?php echo $issue['iss_title']; ?></td></tr>
-	<tr><td style="font-weight: bold">Equipment</td><td><a href="/equipment/view/<?php echo['iss_eq_id']; ?>"><?php echo $issue['eq_name']; ?></a></td></tr>
+	<tr><td style="font-weight: bold">Equipment</td><td><a href="/equipment/view/<?php echo['eq_id']; ?>"><?php echo $issue['eq_name']; ?></a></td></tr>
 	<tr><td style="font-weight: bold">Status</td><td><?php echo $issue['iss_status']; ?></td></tr>
 	<tr><td style="font-weight: bold">Description</td><td><?php echo $issue['iss_description']; ?></td></tr>
-	<tr><td style="font-weight: bold">Reported By</td><td><?php echo $issue['iss_reported_by']; ?></td></tr>
+	<tr><td style="font-weight: bold">Reported By</td><td><?php echo $issue['iss_reported_id']; ?></td></tr>
 	<tr><td style="font-weight: bold">Assigned User</td><td><?php echo $issue['iss_assigned_user']; ?></td></tr>
 	<tr><td style="font-weight: bold">Reported Date</td><?php echo '<td style="font-weight: bold"><script type="text/javascript">var rp_date; $rp_date = moment(\''.$issue['iss_reported_date'].'\').format(\'DD-MM-YYYY\'); document.write($rp_date);</script></td>'; ?></tr>
 	<tr><td style="font-weight: bold">Resolution Date</td><?php echo '<td style="font-weight: bold"><script type="text/javascript">var res_date; $res_date = moment(\''.$issue['iss_resolution_date'].'\').format(\'DD-MM-YYYY\'); document.write($res_date);</script></td>'; ?></tr>
