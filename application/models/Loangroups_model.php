119265f3629d7920bcaad9930f166b897a583f7c
paralittic <ross.wallace@stratis-media.com>
2017-05-19 17:15:03 +1000

    Remove alias

--- application/models/Loangroups_model.php
+++ application/models/Loangroups_model.php
@@ -10,7 +10,7 @@
 				$this->db->order_by('loangroup_created_date', 'ASC');
 				$this->db->join('members', 'members.member_id = loangroups.loangroup_member');
 				$this->db->join('users', 'users.id = loangroups.loangroup_last_modified_user');
-				$this->db->join('users AS musers', 'musers.id = loangroups.loangroup_created_by');
+				$this->db->join('users', 'users.id = loangroups.loangroup_created_by');
 
 				$query = $this->db->get('loangroups');
 				return $query->result_array();
