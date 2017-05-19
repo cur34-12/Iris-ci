abc87456fb68f5777cd695c962010079619403c8
paralittic <ross.wallace@stratis-media.com>
2017-05-19 17:00:01 +1000

    Moving jions outside of if statement

--- application/models/Loangroups_model.php
+++ application/models/Loangroups_model.php
@@ -16,6 +16,8 @@
 				return $query->result_array();
 			}
 			$this->db->join('members', 'members.member_id = loangroups.loangroup_member');
+			$this->db->join('users', 'users.id = loangroups.loangroup_last_modified_user');
+			$this->db->join('users AS modifiedusers', 'modifiedusers.id = loangroups.loangroup_created_by');
 			$query = $this->db->get_where('loangroups', array('loangroup_id' => $loangroup_id));
 			return $query->row_array();
 		}
