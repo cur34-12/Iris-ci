2b87067f7b7b0e34b3a9269c294e23ee46855a91
paralittic <ross.wallace@stratis-media.com>
2017-05-19 17:02:06 +1000

    Shortening alias

--- application/views/loangroups/view.php
+++ application/views/loangroups/view.php
@@ -10,7 +10,7 @@
 	<tr><td style="font-weight: bold">Loan Group Last Modified</td><td><?php echo $loangroup['loangroup_last_modified']; ?></a></td></tr>
 	<tr><td style="font-weight: bold">Loan Group Last Modified by</td><td><?php echo $loangroup['username']; ?></td></tr>
 	<tr><td style="font-weight: bold">Loan Group Created Date</td><td><?php echo $loangroup['loangroup_created_date']; ?></td></tr>
-	<tr><td style="font-weight: bold">Loan Group Created By</td><td><?php echo $loangroup['modifiedusers.username']; ?></td></tr>
+	<tr><td style="font-weight: bold">Loan Group Created By</td><td><?php echo $loangroup['musers.username']; ?></td></tr>
 </table>
 
 <hr />
