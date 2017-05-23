5d408af0150e469740951301961a4d62219e368a
wwwillis <rego@wwwillis.com>
2017-05-23 18:21:08 +1000

    Equipment issue v5

--- application/views/equipment/view.php
+++ application/views/equipment/view.php
@@ -21,8 +21,14 @@
 	<tr><td style="font-weight: bold">Serial</td><td><?php echo $equipment['eq_serial']; ?></td></tr>
 	<tr><td style="font-weight: bold">Barcode</td><td><img src='<?php $bar = new BARCODE(); echo $bar->BarCode_link("Code39", $equipment['eq_id'], 50, 1, "#ffffff", "#000000"); ?>' /></td></tr>
 	<tr><td style="font-weight: bold">QR</td><td><img src='<?php $bar = new BARCODE(); echo $bar->QRCode_link('text', site_url('/equipment/'. $equipment['eq_id']), 100, 2); ?>' /></td></tr>
+	<tr><td style="font-weight: bold">Open Issues</td><td><ul><?php
+	 //echo implode(",",$issues); 
+	foreach($issues as $issues) {
+		?><li><?php implode(",",$issue) ?></li>	<?php
+	}
+
+	 ?></ul> (note this is currently 'all' not just 'open'</td></tr>
 </table>
-Issue test:<?php echo implode(",",$issues); ?>
 <hr />
 <a class="btn btn-default" role="button" href="<?php echo site_url('/equipment/create'); ?>">Create Equipment</a>
 <a class="btn btn-info" role="button" href="<?php echo site_url('/equipment/edit/'. $equipment['eq_id']); ?>">Edit Equipment</a>
