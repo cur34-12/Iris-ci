e9f09f8ed36589f68f0ce740f43898d2ecbf2026
paralittic <ross.wallace@stratis-media.com>
2017-05-23 12:04:58 +1000

    Renamed id to what it should be

--- application/views/equipment/update.php
+++ application/views/equipment/update.php
@@ -9,7 +9,7 @@
 	<div class="form-group">
 		<label class="control-label col-sm-2">Equipment ID</label>
 		<div class="col-sm-10">
-			<input type="text" id="disabledInput" class="form-control" name="eq_id" value="<?php echo $equipment['eq_id']; ?>" disabled>
+			<input type="text" id="eq_id" class="form-control" name="eq_id" value="<?php echo $equipment['eq_id']; ?>" disabled>
 		</div>
 	</div>
 	<div class="form-group">
