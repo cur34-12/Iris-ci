e71082365ebaef4d1271c25037a584d60b26ab0a
paralittic <ross.wallace@stratis-media.com>
2017-05-24 14:44:47 +1000

    Fixed arr name

--- application/views/equipment/index.php
+++ application/views/equipment/index.php
@@ -29,7 +29,7 @@
 			<tr>
 					<td><a href="<?php echo site_url('/equipment/'. $equipment['eq_id']); ?>"><?php echo $equipment['eq_name']; ?></a></td>
 					<td><?php echo $equipment['eq_description']; ?></td>
-					<td><a href="<?php echo site_url('/equipment/group/'. $equipment['eqgroup_id']); ?>"><?php echo $equipment['eqgroup_name']; ?></td>
+					<td><a href="<?php echo site_url('/equipment-groups/'. $equipment['eqgroup_id']); ?>"><?php echo $equipment['eqgroup_name']; ?></td>
 					<td><?php echo $equipment['eq_category']; ?></td>
 					<td><?php echo $equipment['eq_consumable']; ?></td>
 					<td><?php echo $equipment['eq_size']; ?></td>
