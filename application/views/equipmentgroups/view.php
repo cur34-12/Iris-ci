8c75907d4f87dd097734334590ddf78f2ac20f6c
paralittic <ross.wallace@stratis-media.com>
2017-05-24 13:52:00 +1000

    Fixed the view table

--- application/views/equipmentgroups/view.php
+++ application/views/equipmentgroups/view.php
@@ -8,12 +8,12 @@
 <h2 style="text-transform: uppercase;">Equipment Group - <?php echo $equipmentgroup['eqgroup_name']; ?></h2>
 
 <table class="table table-striped table-hover ">
-	<tr><td style="font-weight: bold"><?php echo $equipmentgroup['eqgroup_name']; ?></a></td>
-	<tr><td style="font-weight: bold"><?php echo $equipmentgroup['eqgroup_description']; ?></td>
-	<tr><td style="font-weight: bold"><?php echo $equipmentgroup['eqgroup_category']; ?></td>
-	<tr><td style="font-weight: bold"><?php echo $equipmentgroup['eqgroup_inspection_frequency']; ?></td>
-	<tr><td style="font-weight: bold"><?php echo $equipmentgroup['eqgroup_last_checked']; ?></td>
-	<tr><td style="font-weight: bold"><?php echo $equipmentgroup['eqgroup_location']; ?></td>		
+	<tr><td style="font-weight: bold">Name</td><td><?php echo $equipmentgroup['eqgroup_name']; ?></a></td>
+	<tr><td style="font-weight: bold">Description</td><td><?php echo $equipmentgroup['eqgroup_description']; ?></td>
+	<tr><td style="font-weight: bold">Category</td><td><?php echo $equipmentgroup['eqgroup_category']; ?></td>
+	<tr><td style="font-weight: bold">Inspection Frequency</td><td><?php echo $equipmentgroup['eqgroup_inspection_frequency']; ?></td>
+	<tr><td style="font-weight: bold">Last Checked</td><td><?php echo $equipmentgroup['eqgroup_last_checked']; ?></td>
+	<tr><td style="font-weight: bold">Location</td><td><?php echo $equipmentgroup['eqgroup_location']; ?></td>		
 	<tr><td style="font-weight: bold">Barcode</td><td><img src='<?php $bar = new BARCODE(); echo $bar->BarCode_link("Code39", $equipmentgroup['eqgroup_id'], 50, 1, "#ffffff", "#000000"); ?>' /></td></tr>
 	<tr><td style="font-weight: bold">QR</td><td><img src='<?php $bar = new BARCODE(); echo $bar->QRCode_link('text', site_url('/equipment-groups/'. $equipmentgroup['eqgroup_id']), 100, 2); ?>' /></td></tr>
 </table>
