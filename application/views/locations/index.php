e06e9b6eadf6a5fa60a3b6f1d8066532f81d3a94
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-05-30 11:21:23 +1000

    Fixed bad URI

--- application/views/locations/index.php
+++ application/views/locations/index.php
@@ -8,12 +8,11 @@
 			<th>Edit</th>
 		</tr>
 	</thead>
-
 	<tbody>
 		<?php foreach($locations as $location) : ?>
 			<tr>
 					<td><a href="<?php echo site_url('/locations/'. $location['loc_id']); ?>"><?php echo $location['loc_id']; ?></a></td>
-					<td><a href="<?php echo site_url('/locations/'. $location['loc_name']); ?>"><?php echo $location['loc_name']; ?></td>
+					<td><a href="<?php echo site_url('/locations/'. $location['loc_id']); ?>"><?php echo $location['loc_name']; ?></td>
 					<td><?php echo $location['loc_manager']; ?></td>
 					<td><a class="btn btn-primary btn-sm" role="button" href="locations/edit/<?php echo $location['loc_id']; ?>">Edit</a></td>
 			</tr>
