dc70fe296c4adb24ff442c5713bc59f220ce5092
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-02 09:42:10 +1000

    Fixed relative uri issue

--- application/views/equipment/index.php
+++ application/views/equipment/index.php
@@ -38,7 +38,7 @@
     </table>
 </div>
 
-<a class="btn btn-default" role="button" href="equipment/create"><i class="fa fa-plus-square-o" aria-hidden="true"></i> Create</a>
+<a class="btn btn-default" role="button" href="/equipment/create"><i class="fa fa-plus-square-o" aria-hidden="true"></i> Create</a>
 <!--<a class="btn btn-info" role="button" href="equipment/edit">Edit Equipment</a>-->
 <!--<a class="btn btn-danger" role="button" href="equipment/delete">Delete Equipment</a> -->
 
