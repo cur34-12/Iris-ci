645dd8bc57fcbe74ad8d4d1b6872a144456f30a9
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-02 09:54:10 +1000

    Global confirmation js

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,5 +2,12 @@
                 <br />
                	<br />
                 <div class="well well-sm"><em>Stratis Media &copy; 2017</em></div>
+
+                <script>
+                    $('[data-toggle=confirmation]').confirmation({
+                        rootSelector: '[data-toggle=confirmation]',
+                        // other options
+                    });
+                </script>
         </body>
 </html>
