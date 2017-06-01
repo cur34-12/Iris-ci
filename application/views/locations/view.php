505ed2a93fa217b409b2596750c594d485a44e6e
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-01 16:09:29 +1000

    Spacing

--- application/views/locations/view.php
+++ application/views/locations/view.php
@@ -31,18 +31,10 @@
     </tbody>
 </table>
 
-
-
 <hr />
-<!--Action buttons-->
 
-<!--Create Button buttons-->
 <a class="btn btn-default" role="button" href="<?php echo site_url('/locations/create'); ?>">Create Location</a>
-
-<!--Edit Button-->
 <a class="btn btn-info" role="button" href="<?php echo site_url('/locations/edit/'. $location['loc_id']); ?>">Edit Location</a>
-
-<!--Delete Button (as form to protect against abuse)-->
 <?php $attributes = array('class' => 'form-inline', 'style' => 'display: inline;'); ?>
 <?php echo form_open('/locations/delete/'.$location['loc_id'], $attributes); ?>
 	<input type="submit" value="Delete Location" class="btn btn-danger"/>
