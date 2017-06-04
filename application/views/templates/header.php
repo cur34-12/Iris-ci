af78f7b397a2a32b7648304718fac6c3857a8957
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-04 23:17:35 +1000

    Cleaning up nav

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -55,10 +55,13 @@
         </head>
         <body>
 
-            <nav class="navbar navbar-inverse bg-primary">
-                <a class="navbar-brand" href="<?php echo base_url(); ?>">LOGI</a>
-
-                    <ul class="nav navbar-nav">
+        <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo base_url(); ?>">LOGI</a>
+                <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
                             <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
                         </li>
@@ -141,4 +144,5 @@
                         </li>
                         <?php endif ?>
                     </ul>
+                </div>
             </nav>
