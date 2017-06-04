7209d1e8fa0abdb2b5a8eb720da1d0db0e3536ee
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-04 20:15:41 +1000

    Wrapping contents in responsive div

--- application/views/equipment/index.php
+++ application/views/equipment/index.php
@@ -1,41 +1,43 @@
 <head>
 	<title>Equipment - Logi</title>
 </head>
-<h2 style="text-transform: uppercase;">Equipment</h2>
-<div class="table-responsive">
-    <table id="content" class="table table-striped table-hover logiTable">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Group</th>
-                <th>Category</th>
-                <th>Consumable</th>
-                <th>Size</th>
-                <th>Brand</th>
-                <th>In Service</th>
-                <th>Model</th>
-                <th>Edit</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            <?php foreach($equipment as $equipment) : ?>
+<div class="container-fluid">
+    <h2 style="text-transform: uppercase;">Equipment</h2>
+    <div class="table-responsive">
+        <table id="content" class="table table-striped table-hover logiTable">
+            <thead>
                 <tr>
-                        <td><a href="<?php echo site_url('/equipment/'. $equipment['eq_id']); ?>"><?php echo $equipment['eq_name']; ?></a></td>
-                        <td><?php echo $equipment['eq_description']; ?></td>
-                        <td><a href="<?php echo site_url('/equipment-groups/'. $equipment['eqgroup_id']); ?>"><?php echo $equipment['eqgroup_name']; ?></td>
-                        <td><?php echo $equipment['eq_category']; ?></td>
-                        <td><?php echo $equipment['eq_consumable']; ?></td>
-                        <td><?php echo $equipment['eq_size']; ?></td>
-                        <td><?php echo $equipment['eq_brand']; ?></td>
-                        <td><?php echo $equipment['eq_in_service']; ?></td>
-                        <td><?php echo $equipment['eq_model']; ?></td>
-                        <td><a class="btn btn-primary btn-sm" role="button" href="equipment/edit/<?php echo $equipment['eq_id']; ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a></td>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Group</th>
+                    <th>Category</th>
+                    <th>Consumable</th>
+                    <th>Size</th>
+                    <th>Brand</th>
+                    <th>In Service</th>
+                    <th>Model</th>
+                    <th>Edit</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+
+            <tbody>
+                <?php foreach($equipment as $equipment) : ?>
+                    <tr>
+                            <td><a href="<?php echo site_url('/equipment/'. $equipment['eq_id']); ?>"><?php echo $equipment['eq_name']; ?></a></td>
+                            <td><?php echo $equipment['eq_description']; ?></td>
+                            <td><a href="<?php echo site_url('/equipment-groups/'. $equipment['eqgroup_id']); ?>"><?php echo $equipment['eqgroup_name']; ?></td>
+                            <td><?php echo $equipment['eq_category']; ?></td>
+                            <td><?php echo $equipment['eq_consumable']; ?></td>
+                            <td><?php echo $equipment['eq_size']; ?></td>
+                            <td><?php echo $equipment['eq_brand']; ?></td>
+                            <td><?php echo $equipment['eq_in_service']; ?></td>
+                            <td><?php echo $equipment['eq_model']; ?></td>
+                            <td><a class="btn btn-primary btn-sm" role="button" href="equipment/edit/<?php echo $equipment['eq_id']; ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <a class="btn btn-default" role="button" href="/equipment/create"><i class="fa fa-plus-square-o" aria-hidden="true"></i> Create</a>
