f98c3ee78a863f8d96aa97b43961c19d3c44b307
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-04 22:22:32 +1000

    Fixing up HTML comments

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,16 +8,16 @@
             <!-- Jquery / UI -->
             <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
             <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<!--
-            !-- Full bootstrap js plugins--
-            <script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.js"></script>
 
-            !-- Bootstrap Theme --
-            <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.css">
--->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+            <!-- Full bootstrap js plugins-->
+            <!--<script type="text/javascript" src="<?php // echo base_url(); ?>js/bootstrap.js"></script>-->
+
+            <!-- Bootstrap Theme -->
+            <!--<link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.css">-->
+
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
             <!-- JS Confirmation Dialogue -->
             <script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap-confirmation.js"></script>
