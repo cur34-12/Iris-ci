00975bdbd228141b6f0a7285adc488d825af7403
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-04 23:42:47 +1000

    Splitting header

---
+++ application/views/templates/nav-resources.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Setup the responsive viewport stuff-->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <!-- JQuery / UI -->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/css/bootstrap.min.css">
+        <script src="<?php echo base_url(); ?>js/tether.min.js"></script>
+        <script src="<?php echo base_url(); ?>/js/bootstrap.js"></script>
+
+        <!-- JS Confirmation Dialogue -->
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap-confirmation.js"></script>
+
+        <!-- JS for DateTimePicker -->
+        <!-- JS Time Parser-->
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/moment.js"></script>
+
+        <!-- JS/CSS for DataTables -->
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/autofill/2.2.0/js/dataTables.autoFill.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/autofill/2.2.0/js/autoFill.bootstrap4.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap4.min.js">
+        <script type="stylesheet" src="https://cdn.datatables.net/autofill/2.2.0/css/autoFill.dataTables.min.css"></script>
+
+        <!-- Jquery UI Style -->
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- Custom CSS -->
+        <!--<link rel="stylesheet" href="<?php //echo base_url(); ?>css/style.css">-->
+
+        <!-- Init of the Date Parser-->
+        <script type="text/javascript">
+            moment().format();
+        </script>
+
+    </head>
+    <body>
