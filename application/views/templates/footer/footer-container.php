c14e60b8cfd89f4f9b1f227d85a65a10547559ad
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-04 23:54:32 +1000

    Header structure change

---
+++ application/views/templates/footer/footer-container.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rwallace
+ * Date: 4/06/17
+ * Time: 11:50 PM
+ */
--- application/views/templates/footer/footer.php
+++ application/views/templates/footer/footer.php
@@ -1,8 +1,4 @@
 				</div>
-                <br />
-               	<br />
-                <div class="well well-sm"><em>Stratis Media &copy; 2017</em></div>
-
                 <script>
                     $('[data-toggle=confirmation]').confirmation({
                         rootSelector: '[data-toggle=confirmation]',
