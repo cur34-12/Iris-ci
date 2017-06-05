a33ba50da5e7b1aa1eed527e956c84032032cffe
paralittic <ross.wallace@stratis-media.com>
2017-06-05 10:11:41 +1000

    New templates

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -2,16 +2,18 @@
         class Pages extends CI_Controller {
                 public function view($page = 'home')
                 {
-        	       if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-        	       {
-                        // Whoops, we don't have a page for that!
-                        show_404();
-        	       }
+                        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+                        {
+                                show_404();
+                        }
 
                         $data['title'] = ucfirst($page); // Capitalize the first letter               
 
-        	       $this->load->view('templates/header', $data);
-        	       $this->load->view('pages/'.$page, $data);
-        	       $this->load->view('templates/footer', $data);
+                        $this->load->view('templates/header/header-resources-std', $data);
+                        $this->load->view('templates/header/header-nav');
+                        $this->load->view('templates/header/header-container');
+                        $this->load->view('pages/', $data);
+                        $this->load->view('templates/footer/footer-container');
+                        $this->load->view('templates/footer/footer-required');
                 }
         }
