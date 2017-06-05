e796e49b2dcc69c663f59257e15d41a7a8e3e264
paralittic <ross.wallace@stratis-media.com>
2017-06-05 16:46:25 +1000

    Cleaning templates

--- application/controllers/Unitmembers.php
+++ application/controllers/Unitmembers.php
@@ -1,11 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+        //Members
 
         class Unitmembers extends CI_Controller {
                 public function index(){
-                        $data['title'] = 'Members'; 
 
-                        $data['members'] = $this->Unitmembers_model->get_members();
+                    $data['title'] = 'Members'; 
+
+                    $data['members'] = $this->Unitmembers_model->get_members();
                         
                         $this->load->view('templates/header', $data);
                         $this->load->view('members/index', $data);
@@ -13,72 +15,96 @@
                 }
 
                 public function view($member_id = null){
-                        $data['member'] = $this->Unitmembers_model->get_members($member_id);
 
-                        if(empty($data['member'])){
-                                show_404();
-                        }
+                    $data['member'] = $this->Unitmembers_model->get_members($member_id);
 
-                        $data['title'] = $data['member']['member_name'];
+                    if(empty($data['member'])){
+                            show_404();
+                    }
 
-                        $this->load->view('templates/header', $data);
-                        $this->load->view('members/view', $data);
-                        $this->load->view('templates/footer');
+                    $data['title'] = $data['member']['member_name'];
+
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
+                    $this->load->view('members/view', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function create(){
-                        $data['title'] = 'Create member';
 
-                        if($this->form_validation->run() === FALSE){
-                                $this->load->view('templates/header', $data);
-                                $this->load->view('members/create', $data);
-                                $this->load->view('templates/footer');  
-                        } else {
-                                $this->Unitmembers_model->create_member();
-                                redirect('members');     
-                        }
+                    $data['title'] = 'Create member';
+
+                    if($this->form_validation->run() === FALSE){
+                        $this->load->view('templates/header/header-resources-std', $data);
+                        $this->load->view('templates/header/header-nav');
+                        $this->load->view('templates/header/header-container');
+                        $this->load->view('members/create', $data);
+                        $this->load->view('templates/footer/footer-container');
+                        $this->load->view('templates/footer/footer-required');  
+                    } else {
+                            $this->Unitmembers_model->create_member();
+                            redirect('members');     
+                    }
                 }
 
                 public function delete($member_id){
+
                         $this->Unitmembers_model->delete_vehicle($member_id);
                         redirect('vehicles');
                 }
 
                 public function edit($member_id){
-                        $data['member'] = $this->Unitmembers_model->get_members($member_id);
 
-                        if(empty($data['member'])){
-                                show_404();
-                        }
+                    $data['member'] = $this->Unitmembers_model->get_members($member_id);
 
-                        $data['title'] = 'Edit member';
+                    if(empty($data['member'])){
+                            show_404();
+                    }
 
-                        $this->load->view('templates/header', $data);
-                        $this->load->view('members/update', $data);
-                        $this->load->view('templates/footer');
+                    $data['title'] = 'Edit member';
+
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
+                    $this->load->view('members/update', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function update(){
-                        $this->Unitmembers_model->update_member();
-                        redirect('members');
+
+                    $this->Unitmembers_model->update_member();
+                    redirect('members');
                 }
 
                 public function select_edit(){ 
 
                     $data['members'] = $this->Unitmembers_model->get_members();
+
+                    $data['title'] = 'Logi - Edit';
                         
-                    $this->load->view('templates/header', $data);
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
                     $this->load->view('members/edit', $data);
-                    $this->load->view('templates/footer');
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function select_delete(){ 
 
                     $data['members'] = $this->Unitmembers_model->get_members();
+
+                    $data['title'] = 'Logi - Delete';
                     
-                    $this->load->view('templates/header', $data);
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
                     $this->load->view('members/delete', $data);
-                    $this->load->view('templates/footer');
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
         }
 ?>
