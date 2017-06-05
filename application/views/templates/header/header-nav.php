b11d28a4c51e0c98adc58d5e223f3ad9ff90dab6
paralittic <ross.wallace@stratis-media.com>
2017-06-05 11:22:28 +1000

    Search button color

--- application/views/templates/header/header-nav.php
+++ application/views/templates/header/header-nav.php
@@ -62,7 +62,7 @@
         </ul>
         <form class="form-inline">
             <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+            <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
         </form>
         <ul class="navbar-nav">
             <?php if ($this->ion_auth->logged_in()): ?>
