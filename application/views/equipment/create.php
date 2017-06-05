7b6d444c897fd86a4b782158348f7b19d4bfccf2
wwwillis <rego@wwwillis.com>
2017-06-05 21:13:58 +1000

    wrong plode

--- application/views/equipment/create.php
+++ application/views/equipment/create.php
@@ -155,7 +155,7 @@
 	</div>
 	<div class="form-group">
 		<label class="control-label col-sm-2">Location</label>
-		<?php echo explode(",",$equipmentlocations) ?>
+		<?php echo implode(",",$equipmentlocations) ?>
 		<div class="col-sm-10">
 			<select id="eqloc_loc_id" name="eqloc_loc_id" class="form-control">
 				<?php foreach($equipmentlocations as $equipmentlocation): ?>
