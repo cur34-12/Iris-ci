b74c336aea112194915a4b4d0447a8601063f8d3
paralittic <ross.wallace@stratis-media.com>
2017-06-05 13:52:46 +1000

    Spacing and new templates

--- application/controllers/Locations.php
+++ application/controllers/Locations.php
@@ -5,87 +5,87 @@
         class Locations extends CI_Controller {
                 public function index(){
 
-                        $data['locations'] = $this->Locations_model->get_locations();
+                    $data['locations'] = $this->Locations_model->get_locations();
 
-                        $data['title'] = 'Locations';
-                        
-                        $this->load->view('templates/header/header-resources-datatable', $data);
-                        $this->load->view('templates/header/header-nav');
-                        $this->load->view('templates/header/header-container-95');
-                        $this->load->view('locations/index', $data);
-                        $this->load->view('templates/footer/footer-container');
-                        $this->load->view('templates/footer/footer-required');
+                    $data['title'] = 'Locations';
+                    
+                    $this->load->view('templates/header/header-resources-datatable', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container-95');
+                    $this->load->view('locations/index', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function view($loc_id = null){
 
-                        $data['location'] = $this->Locations_model->get_locations($loc_id);
-                        $data['equipment'] = $this->Equipment_model->get_location_equipment($loc_id);
+                    $data['location'] = $this->Locations_model->get_locations($loc_id);
+                    $data['equipment'] = $this->Equipment_model->get_location_equipment($loc_id);
 
-                        if(empty($data['location'])){
-                                show_404();
-                        }
+                    if(empty($data['location'])){
+                            show_404();
+                    }
 
-                        $data['title'] = $data['location']['loc_name'];
+                    $data['title'] = $data['location']['loc_name'];
 
-                        $this->load->view('templates/header/header-resources-std', $data);
-                        $this->load->view('templates/header/header-nav');
-                        $this->load->view('templates/header/header-container');
-                        $this->load->view('locations/view', $data);
-                        $this->load->view('templates/footer/footer-container');
-                        $this->load->view('templates/footer/footer-required');
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
+                    $this->load->view('locations/view', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function create(){
                         
-                        $data['usernames'] = $this->User_model->get_users();
+                    $data['usernames'] = $this->User_model->get_users();
 
-                        $data['title'] = 'Create location';
+                    $data['title'] = 'Create location';
 
-                        $this->form_validation->set_rules('loc_name', 'Location Name', 'required');
+                    $this->form_validation->set_rules('loc_name', 'Location Name', 'required');
 
-                        if($this->form_validation->run() === FALSE){
-                                $this->load->view('templates/header/header-resources-std', $data);
-                                $this->load->view('templates/header/header-nav');
-                                $this->load->view('templates/header/header-container');
-                                $this->load->view('locations/create', $data);
-                                $this->load->view('templates/footer/footer-container');
-                                $this->load->view('templates/footer/footer-required');  
-                        } else {
-                                $this->Locations_model->create_location();
-                                redirect('locations');
-                        }
+                    if($this->form_validation->run() === FALSE){
+                            $this->load->view('templates/header/header-resources-std', $data);
+                            $this->load->view('templates/header/header-nav');
+                            $this->load->view('templates/header/header-container');
+                            $this->load->view('locations/create', $data);
+                            $this->load->view('templates/footer/footer-container');
+                            $this->load->view('templates/footer/footer-required');  
+                    } else {
+                            $this->Locations_model->create_location();
+                            redirect('locations');
+                    }
                 }
 
                 public function delete($loc_id){
 
-                        $this->Locations_model->delete_location($loc_id);
-                        redirect('locations');
+                    $this->Locations_model->delete_location($loc_id);
+                    redirect('locations');
                 }
 
                 public function edit($loc_id){
                     
-                        $data['location'] = $this->Locations_model->get_locations($loc_id);
-                        $data['usernames'] = $this->User_model->get_users();
+                    $data['location'] = $this->Locations_model->get_locations($loc_id);
+                    $data['usernames'] = $this->User_model->get_users();
 
-                        if(empty($data['location'])){
-                                show_404();
-                        }
+                    if(empty($data['location'])){
+                            show_404();
+                    }
 
-                        $data['title'] = 'Edit location';
+                    $data['title'] = 'Edit location';
 
-                        $this->load->view('templates/header/header-resources-std', $data);
-                        $this->load->view('templates/header/header-nav');
-                        $this->load->view('templates/header/header-container');
-                        $this->load->view('locations/update', $data);
-                        $this->load->view('templates/footer/footer-container');
-                        $this->load->view('templates/footer/footer-required');
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
+                    $this->load->view('locations/update', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function update(){
 
-                        $this->Locations_model->update_location();
-                        redirect('locations');
+                    $this->Locations_model->update_location();
+                    redirect('locations');
                 }
 
                 public function select_edit(){ 
--- application/controllers/Suppliers.php
+++ application/controllers/Suppliers.php
@@ -1,82 +1,113 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+        //Suppliers
+
         class Suppliers extends CI_Controller {
                 public function index(){
-                        $data['title'] = 'Supliers'; 
 
-                         $data['suppliers'] = $this->Suppliers_model->get_suppliers();
-                        
-        	       $this->load->view('templates/header', $data);
-        	       $this->load->view('suppliers/index', $data);
-        	       $this->load->view('templates/footer');
+                    $data['title'] = 'Supliers'; 
+
+                    $data['suppliers'] = $this->Suppliers_model->get_suppliers();
+                    
+                    $this->load->view('templates/header/header-resources-datatable', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container-95');
+                    $this->load->view('suppliers/index', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function view($supplier_id = null){
-                        $data['supplier'] = $this->Suppliers_model->get_suppliers($supplier_id);
 
-                        if(empty($data['supplier'])){
-                                show_404();
-                        }
+                    $data['supplier'] = $this->Suppliers_model->get_suppliers($supplier_id);
 
-                        $data['title'] = $data['supplier']['supplier_name'];
+                    if(empty($data['supplier'])){
+                            show_404();
+                    }
 
-                        $this->load->view('templates/header', $data);
-                        $this->load->view('suppliers/view', $data);
-                        $this->load->view('templates/footer');
+                    $data['title'] = $data['supplier']['supplier_name'];
+
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
+                    $this->load->view('suppliers/view', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required');
                 }
 
                 public function create(){
-                        $data['title'] = 'Create Supplier';
 
-                        if($this->form_validation->run() === FALSE){
-                                $this->load->view('templates/header', $data);
-                                $this->load->view('suppliers/create', $data);
-                                $this->load->view('templates/footer');  
-                        } else {
-                                $this->Suppliers_model->create_supplier();
-                                redirect('suppliers');     
-                        }
+                    $data['title'] = 'Create Supplier';
+
+                    if($this->form_validation->run() === FALSE){
+                        $this->load->view('templates/header/header-resources-std', $data);
+                        $this->load->view('templates/header/header-nav');
+                        $this->load->view('templates/header/header-container');
+                        $this->load->view('suppliers/create', $data);
+                        $this->load->view('templates/footer/footer-container');
+                        $this->load->view('templates/footer/footer-required'); 
+                    } else {
+                        $this->Suppliers_model->create_supplier();
+                        redirect('suppliers');     
+                    }
                 }
 
                 public function delete($supplier_id){
-                        $this->Suppliers_model->delete_vehicle($supplier_id);
-                        redirect('vehicles');
+
+                    $this->Suppliers_model->delete_vehicle($supplier_id);
+                    redirect('vehicles');
                 }
 
                 public function edit($supplier_id){
-                        $data['supplier'] = $this->Suppliers_model->get_suppliers($supplier_id);
 
-                        if(empty($data['supplier'])){
-                                show_404();
-                        }
+                    $data['supplier'] = $this->Suppliers_model->get_suppliers($supplier_id);
 
-                        $data['title'] = 'Edit Supplier';
+                    if(empty($data['supplier'])){
+                            show_404();
+                    }
 
-                        $this->load->view('templates/header', $data);
-                        $this->load->view('suppliers/update', $data);
-                        $this->load->view('templates/footer');
+                    $data['title'] = 'Edit Supplier';
+
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
+                    $this->load->view('suppliers/update', $data);
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required'); 
                 }
 
                 public function update(){
-                        $this->Suppliers_model->update_supplier();
-                        redirect('suppliers');
+
+                    $this->Suppliers_model->update_supplier();
+                    redirect('suppliers');
                 }
 
                 public function select_edit(){ 
 
                     $data['suppliers'] = $this->Suppliers_model->get_suppliers();
+
+                    $data['title'] = 'Logi - Edit';
                         
-                    $this->load->view('templates/header', $data);
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
                     $this->load->view('suppliers/edit', $data);
-                    $this->load->view('templates/footer');
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required'); 
                 }
 
                 public function select_delete(){ 
 
                     $data['suppliers'] = $this->Suppliers_model->get_suppliers();
+
+                    $data['title'] = 'Logi - Delete';
                     
-                    $this->load->view('templates/header', $data);
+                    $this->load->view('templates/header/header-resources-std', $data);
+                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-container');
                     $this->load->view('suppliers/delete', $data);
-                    $this->load->view('templates/footer');3wa 
+                    $this->load->view('templates/footer/footer-container');
+                    $this->load->view('templates/footer/footer-required'); 
                 }
         }
 ?>
