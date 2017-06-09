37c6a6724a7343f4361759fc48b23048b3157f6c
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-10 01:30:02 +1000

    no container

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,19 +1,17 @@
 <h1>Welcome to <strong>Logi</strong>, an SES focused logistics resource planning system</h1>
 <!--Action boxes-->
-<div class="container-fluid">
-    <div class="quick-actions_homepage">
-        <ul class="quick-actions">
-            <li class="bg_lb"> <a href="index.html"> <span class="glyphicon glyphicon-dashboard"></span> My Dashboard </a> </li>
-            <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-signal"></i> Charts</a> </li>
-            <li class="bg_ly"> <a href="widgets.html"> <i class="glyphicon glyphicon-inbox"></i> Widgets </a> </li>
-            <li class="bg_lo"> <a href="tables.html"> <i class="glyphicon glyphicon-th"></i> Tables</a> </li>
-            <li class="bg_ls"> <a href="grid.html"> <i class="glyphicon glyphicon-fullscreen"></i> Full width</a> </li>
-            <li class="bg_lo span3"> <a href="form-common.html"> <i class="glyphicon glyphicon-th-list"></i> Forms</a> </li>
-            <li class="bg_ls"> <a href="buttons.html"> <i class="glyphicon glyphicon-tint"></i> Buttons</a> </li>
-            <li class="bg_lb"> <a href="interface.html"> <i class="glyphicon glyphicon-pencil"></i>Elements</a> </li>
-            <li class="bg_lg"> <a href="calendar.html"> <i class="glyphicon glyphicon-calendar"></i> Calendar</a> </li>
-            <li class="bg_lr"> <a href="error404.html"> <i class="glyphicon glyphicon-info-sign"></i> Error</a> </li>
+<div class="quick-actions_homepage">
+    <ul class="quick-actions">
+        <li class="bg_lb span3"> <a href="index.html"> <span class="glyphicon glyphicon-dashboard"></span> My Dashboard </a> </li>
+        <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-signal"></i> Charts</a> </li>
+        <li class="bg_ly span3"> <a href="widgets.html"> <i class="glyphicon glyphicon-inbox"></i> Widgets </a> </li>
+        <li class="bg_lo span3"> <a href="tables.html"> <i class="glyphicon glyphicon-th"></i> Tables</a> </li>
+        <li class="bg_ls span3"> <a href="grid.html"> <i class="glyphicon glyphicon-fullscreen"></i> Full width</a> </li>
+        <li class="bg_lo span3"> <a href="form-common.html"> <i class="glyphicon glyphicon-th-list"></i> Forms</a> </li>
+        <li class="bg_ls span3"> <a href="buttons.html"> <i class="glyphicon glyphicon-tint"></i> Buttons</a> </li>
+        <li class="bg_lb span3"> <a href="interface.html"> <i class="glyphicon glyphicon-pencil"></i>Elements</a> </li>
+        <li class="bg_lg span3"> <a href="calendar.html"> <i class="glyphicon glyphicon-calendar"></i> Calendar</a> </li>
+        <li class="bg_lr span3"> <a href="error404.html"> <i class="glyphicon glyphicon-info-sign"></i> Error</a> </li>
 
-        </ul>
-    </div>
-    <!--End-Action boxes-->
+    </ul>
+</div>
