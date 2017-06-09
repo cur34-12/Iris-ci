fa9a79bec94809ad1768a4681847f101c68c67d6
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-09 23:55:16 +1000

    condensed table

--- application/views/suppliers/index.php
+++ application/views/suppliers/index.php
@@ -1,6 +1,6 @@
 <h2 style="text-transform: uppercase;">Suppliers</h2>
 <hr />
-<table id="content" class="table table-striped table-hover dataTable">
+<table id="content" class="table table-striped table-hover table-condensed dataTable">
 	<thead class="thead-inverse">
 		<tr>
 			<th>Name</th>
