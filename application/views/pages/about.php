2b583d20f9b54bd2c787537cfe1401acbc50d079
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-09 22:23:01 +1000

    single class now

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,4 +1,4 @@
-<div class="row" class="col-xs-9">
+<div class="row col-xs-9">
 <h1>Logi - An emergency services resource management system</h1>
 <p>Developed with unit based NSW State Emergency Service users in mind Logi aims to create a multifaceted data management system. The main functions sit around the marrying of users/members to equipment loans and time management. The system is build completely with user experience in mind so that minimal training or guidance is required for use from both a manager and end user perspective.</p>
 <hr />
