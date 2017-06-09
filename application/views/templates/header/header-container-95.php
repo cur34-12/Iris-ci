3833f59c7647b9f4cf088b7a63553ffe27c5774d
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-09 19:08:59 +1000

    Adding hamburger to 95%

--- application/views/templates/header/header-container-95.php
+++ application/views/templates/header/header-container-95.php
@@ -1,3 +1,9 @@
 </head>
 <body>
+<div id="page-content-wrapper">
+    <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+        <span class="hamb-top"></span>
+        <span class="hamb-middle"></span>
+        <span class="hamb-bottom"></span>
+    </button>
 <div class="container" style="margin-top: 2%; width: 95%;">
