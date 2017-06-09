e48c40aa4d299ccf485a14e2b360b9f07986f8b9
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-10 01:50:46 +1000

    Icons

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,14 +2,14 @@
 <!--Action boxes-->
 <div class="quick-actions_homepage">
     <ul class="quick-actions">
-        <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-signal"></i> Equipment</a> </li>
-        <li class="bg_ly span3"> <a href="widgets.html"> <i class="glyphicon glyphicon-inbox"></i> Issues </a> </li>
-        <li class="bg_lo span3"> <a href="tables.html"> <i class="glyphicon glyphicon-th"></i> Fleet</a> </li>
-        <li class="bg_ls span3"> <a href="grid.html"> <i class="glyphicon glyphicon-fullscreen"></i> Q Store</a> </li>
-        <li class="bg_lo span3"> <a href="form-common.html"> <i class="glyphicon glyphicon-th-list"></i> Members</a> </li>
-        <li class="bg_ls span3"> <a href="buttons.html"> <i class="glyphicon glyphicon-tint"></i> Suppliers</a> </li>
-        <li class="bg_lb span3"> <a href="interface.html"> <i class="glyphicon glyphicon-pencil"></i> Locations</a> </li>
-        <li class="bg_lg span3"> <a href="calendar.html"> <i class="glyphicon glyphicon-calendar"></i> Dashboards</a> </li>
-        <li class="bg_lr span3"> <a href="error404.html"> <i class="glyphicon glyphicon-info-sign"></i> Reports</a> </li>
+        <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-wrench"></i> Equipment</a> </li>
+        <li class="bg_ly span3"> <a href="widgets.html"> <i class="glyphicon glyphicon-tag"></i> Issues </a> </li>
+        <li class="bg_lo span3"> <a href="tables.html"> <i class="glyphicon glyphicon-road"></i> Fleet</a> </li>
+        <li class="bg_ls span3"> <a href="grid.html"> <i class="glyphicon glyphicon-tags"></i> Q Store</a> </li>
+        <li class="bg_lo span3"> <a href="form-common.html"> <i class="glyphicon glyphicon-user"></i> Members</a> </li>
+        <li class="bg_ls span3"> <a href="buttons.html"> <i class="glyphicon glyphicon-cog"></i> Suppliers</a> </li>
+        <li class="bg_lb span3"> <a href="interface.html"> <i class="glyphicon glyphicon-pictures"></i> Locations</a> </li>
+        <li class="bg_lg span3"> <a href="calendar.html"> <i class="glyphicon glyphicon-stats"></i> Dashboards</a> </li>
+        <li class="bg_lr span3"> <a href="error404.html"> <i class="glyphicon glyphicon-duplicate"></i> Reports</a> </li>
     </ul>
 </div>
