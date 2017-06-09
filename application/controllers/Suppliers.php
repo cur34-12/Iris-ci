813afae2ec8a464b997479005ca7f2e80f92cf03
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-09 19:03:29 +1000

    Sidebar nav for suppliers

--- application/controllers/Suppliers.php
+++ application/controllers/Suppliers.php
@@ -8,13 +8,12 @@
                     $data['title'] = 'Supliers'; 
 
                     $data['suppliers'] = $this->Suppliers_model->get_suppliers();
-                    
-                    $this->load->view('templates/header/header-resources-datatable', $data);
-                    $this->load->view('templates/header/header-nav');
+
+                    $this->load->view('templates/header/header-required', $data);
+                    $this->load->view('templates/header/header-sidebar');
                     $this->load->view('templates/header/header-container-95');
                     $this->load->view('suppliers/index', $data);
                     $this->load->view('templates/footer/footer-datatable');
-                    $this->load->view('templates/footer/footer-container');
                     $this->load->view('templates/footer/footer-required');
                 }
 
@@ -28,11 +27,10 @@
 
                     $data['title'] = $data['supplier']['supplier_name'];
 
-                    $this->load->view('templates/header/header-resources-std', $data);
-                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-required', $data);
+                    $this->load->view('templates/header/header-sidebar');
                     $this->load->view('templates/header/header-container');
                     $this->load->view('suppliers/view', $data);
-                    $this->load->view('templates/footer/footer-container');
                     $this->load->view('templates/footer/footer-required');
                 }
 
@@ -41,11 +39,10 @@
                     $data['title'] = 'Create Supplier';
 
                     if($this->form_validation->run() === FALSE){
-                        $this->load->view('templates/header/header-resources-std', $data);
-                        $this->load->view('templates/header/header-nav');
+                        $this->load->view('templates/header/header-required', $data);
+                        $this->load->view('templates/header/header-sidebar');
                         $this->load->view('templates/header/header-container');
                         $this->load->view('suppliers/create', $data);
-                        $this->load->view('templates/footer/footer-container');
                         $this->load->view('templates/footer/footer-required'); 
                     } else {
                         $this->Suppliers_model->create_supplier();
@@ -69,11 +66,10 @@
 
                     $data['title'] = 'Edit Supplier';
 
-                    $this->load->view('templates/header/header-resources-std', $data);
-                    $this->load->view('templates/header/header-nav');
+                    $this->load->view('templates/header/header-required', $data);
+                    $this->load->view('templates/header/header-sidebar');
                     $this->load->view('templates/header/header-container');
                     $this->load->view('suppliers/update', $data);
-                    $this->load->view('templates/footer/footer-container');
                     $this->load->view('templates/footer/footer-required'); 
                 }
 
@@ -88,12 +84,11 @@
                     $data['suppliers'] = $this->Suppliers_model->get_suppliers();
 
                     $data['title'] = 'Logi - Edit';
-                        
-                    $this->load->view('templates/header/header-resources-std', $data);
-                    $this->load->view('templates/header/header-nav');
+
+                    $this->load->view('templates/header/header-required', $data);
+                    $this->load->view('templates/header/header-sidebar');
                     $this->load->view('templates/header/header-container');
                     $this->load->view('suppliers/edit', $data);
-                    $this->load->view('templates/footer/footer-container');
                     $this->load->view('templates/footer/footer-required'); 
                 }
 
@@ -102,12 +97,11 @@
                     $data['suppliers'] = $this->Suppliers_model->get_suppliers();
 
                     $data['title'] = 'Logi - Delete';
-                    
-                    $this->load->view('templates/header/header-resources-std', $data);
-                    $this->load->view('templates/header/header-nav');
+
+                    $this->load->view('templates/header/header-required', $data);
+                    $this->load->view('templates/header/header-sidebar');
                     $this->load->view('templates/header/header-container');
                     $this->load->view('suppliers/delete', $data);
-                    $this->load->view('templates/footer/footer-container');
                     $this->load->view('templates/footer/footer-required'); 
                 }
         }
