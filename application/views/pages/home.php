d93f932ce6503f4066c14892161975bb0345594f
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-10 01:26:09 +1000

    span vs i

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -4,15 +4,15 @@
     <div class="quick-actions_homepage">
         <ul class="quick-actions">
             <li class="bg_lb"> <a href="index.html"> <span class="glyphicon glyphicon-dashboard"></span> My Dashboard </a> </li>
-            <li class="bg_lg span3"> <a href="charts.html"> <i class="icon-signal"></i> Charts</a> </li>
-            <li class="bg_ly"> <a href="widgets.html"> <i class="icon-inbox"></i> Widgets </a> </li>
-            <li class="bg_lo"> <a href="tables.html"> <i class="icon-th"></i> Tables</a> </li>
-            <li class="bg_ls"> <a href="grid.html"> <i class="icon-fullscreen"></i> Full width</a> </li>
-            <li class="bg_lo span3"> <a href="form-common.html"> <i class="icon-th-list"></i> Forms</a> </li>
-            <li class="bg_ls"> <a href="buttons.html"> <i class="icon-tint"></i> Buttons</a> </li>
-            <li class="bg_lb"> <a href="interface.html"> <i class="icon-pencil"></i>Elements</a> </li>
-            <li class="bg_lg"> <a href="calendar.html"> <i class="icon-calendar"></i> Calendar</a> </li>
-            <li class="bg_lr"> <a href="error404.html"> <i class="icon-info-sign"></i> Error</a> </li>
+            <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-signal"></i> Charts</a> </li>
+            <li class="bg_ly"> <a href="widgets.html"> <i class="glyphicon glyphicon-inbox"></i> Widgets </a> </li>
+            <li class="bg_lo"> <a href="tables.html"> <i class="glyphicon glyphicon-th"></i> Tables</a> </li>
+            <li class="bg_ls"> <a href="grid.html"> <i class="glyphicon glyphicon-fullscreen"></i> Full width</a> </li>
+            <li class="bg_lo span3"> <a href="form-common.html"> <i class="glyphicon glyphicon-th-list"></i> Forms</a> </li>
+            <li class="bg_ls"> <a href="buttons.html"> <i class="glyphicon glyphicon-tint"></i> Buttons</a> </li>
+            <li class="bg_lb"> <a href="interface.html"> <i class="glyphicon glyphicon-pencil"></i>Elements</a> </li>
+            <li class="bg_lg"> <a href="calendar.html"> <i class="glyphicon glyphicon-calendar"></i> Calendar</a> </li>
+            <li class="bg_lr"> <a href="error404.html"> <i class="glyphicon glyphicon-info-sign"></i> Error</a> </li>
 
         </ul>
     </div>
