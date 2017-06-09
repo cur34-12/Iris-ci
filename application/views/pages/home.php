8fe6b094f019aff514c7caeb2462d6b4b7d4bc43
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-10 01:44:10 +1000

    dash gone

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,7 +2,6 @@
 <!--Action boxes-->
 <div class="quick-actions_homepage">
     <ul class="quick-actions">
-        <li class="bg_lb span3"> <a href="index.html"> <i class="glyphicon glyphicon-dashboard"></i> My Dashboard </a> </li>
         <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-signal"></i> Equipment</a> </li>
         <li class="bg_ly span3"> <a href="widgets.html"> <i class="glyphicon glyphicon-inbox"></i> Issues </a> </li>
         <li class="bg_lo span3"> <a href="tables.html"> <i class="glyphicon glyphicon-th"></i> Fleet</a> </li>
@@ -12,6 +11,5 @@
         <li class="bg_lb span3"> <a href="interface.html"> <i class="glyphicon glyphicon-pencil"></i> Locations</a> </li>
         <li class="bg_lg span3"> <a href="calendar.html"> <i class="glyphicon glyphicon-calendar"></i> Dashboards</a> </li>
         <li class="bg_lr span3"> <a href="error404.html"> <i class="glyphicon glyphicon-info-sign"></i> Reports</a> </li>
-
     </ul>
 </div>
