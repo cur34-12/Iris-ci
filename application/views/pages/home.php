e7aa87a1724bac99bfe9e8b5388755be62579065
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-10 01:43:08 +1000

    names

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,15 +3,15 @@
 <div class="quick-actions_homepage">
     <ul class="quick-actions">
         <li class="bg_lb span3"> <a href="index.html"> <i class="glyphicon glyphicon-dashboard"></i> My Dashboard </a> </li>
-        <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-signal"></i> Charts</a> </li>
-        <li class="bg_ly span3"> <a href="widgets.html"> <i class="glyphicon glyphicon-inbox"></i> Widgets </a> </li>
-        <li class="bg_lo span3"> <a href="tables.html"> <i class="glyphicon glyphicon-th"></i> Tables</a> </li>
-        <li class="bg_ls span3"> <a href="grid.html"> <i class="glyphicon glyphicon-fullscreen"></i> Full width</a> </li>
-        <li class="bg_lo span3"> <a href="form-common.html"> <i class="glyphicon glyphicon-th-list"></i> Forms</a> </li>
-        <li class="bg_ls span3"> <a href="buttons.html"> <i class="glyphicon glyphicon-tint"></i> Buttons</a> </li>
-        <li class="bg_lb span3"> <a href="interface.html"> <i class="glyphicon glyphicon-pencil"></i>Elements</a> </li>
-        <li class="bg_lg span3"> <a href="calendar.html"> <i class="glyphicon glyphicon-calendar"></i> Calendar</a> </li>
-        <li class="bg_lr span3"> <a href="error404.html"> <i class="glyphicon glyphicon-info-sign"></i> Error</a> </li>
+        <li class="bg_lg span3"> <a href="charts.html"> <i class="glyphicon glyphicon-signal"></i> Equipment</a> </li>
+        <li class="bg_ly span3"> <a href="widgets.html"> <i class="glyphicon glyphicon-inbox"></i> Issues </a> </li>
+        <li class="bg_lo span3"> <a href="tables.html"> <i class="glyphicon glyphicon-th"></i> Fleet</a> </li>
+        <li class="bg_ls span3"> <a href="grid.html"> <i class="glyphicon glyphicon-fullscreen"></i> Q Store</a> </li>
+        <li class="bg_lo span3"> <a href="form-common.html"> <i class="glyphicon glyphicon-th-list"></i> Members</a> </li>
+        <li class="bg_ls span3"> <a href="buttons.html"> <i class="glyphicon glyphicon-tint"></i> Suppliers</a> </li>
+        <li class="bg_lb span3"> <a href="interface.html"> <i class="glyphicon glyphicon-pencil"></i> Locations</a> </li>
+        <li class="bg_lg span3"> <a href="calendar.html"> <i class="glyphicon glyphicon-calendar"></i> Dashboards</a> </li>
+        <li class="bg_lr span3"> <a href="error404.html"> <i class="glyphicon glyphicon-info-sign"></i> Reports</a> </li>
 
     </ul>
 </div>
