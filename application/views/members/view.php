f3144013d9e25081df86e657dfac342099868b30
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-09 23:15:36 +1000

    200 dollarydoos

--- application/views/members/view.php
+++ application/views/members/view.php
@@ -1,8 +1,9 @@
-<h2><?php echo $member['member_name']); ?></h2>
+<h2><?php echo $member['$member_name']); ?></h2>
+<hr />
 <table class="table table-striped table-hover ">
     <tbody>
-    <tr><td style="font-weight: bold">Member ID</td><td><?php echo $member['member_id']); ?></td></tr>
-    <tr><td style="font-weight: bold">Member Name</td><td><?php echo $member['member_name']; ?></td></tr>
+    <tr><td style="font-weight: bold">Member ID</td><td><?php echo $member['$member_id']); ?></td></tr>
+    <tr><td style="font-weight: bold">Member Name</td><td><?php echo $member['$member_name']; ?></td></tr>
     </tbody>
 </table>
 <hr />
@@ -17,8 +18,8 @@
     <tbody>
         <?php foreach($loanitems as $loanitem) : ?>
             <tr>
-                <td><?php echo $loanitem['loan_something']); ?></td>
-                <td><?php echo $loanitem['loan_something']; ?></td>
+                <td><?php echo $loanitem['$loan_something']); ?></td>
+                <td><?php echo $loanitem['$loan_something']; ?></td>
             </tr>
         <?php endforeach; ?>
     </tbody>
@@ -26,6 +27,6 @@
 <hr />
 
 <a class="btn btn-primary" role="button" href="<?php echo site_url('/members/create'); ?>">Create Member</a>
-<a class="btn btn-warning" role="button" href="<?php echo site_url('/members/edit/'. $member['member_id']); ?>">Edit Member</a>
+<a class="btn btn-warning" role="button" href="<?php echo site_url('/members/edit/'. $member['$member_id']); ?>">Edit Member</a>
 
 
