59656e7734095f440e4b303abf004e1686a2c5fc
Ross Wallace <ross.wallace@stratis-media.com>
2017-06-13 19:06:19 +1000

    yes was needed and main url conf split

--- application/config/config.php
+++ application/config/config.php
@@ -1,29 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-|--------------------------------------------------------------------------
-| Base Site URL
-|--------------------------------------------------------------------------
-|
-| URL to your CodeIgniter root. Typically this will be your base URL,
-| WITH a trailing slash:
-|
-|	http://example.com/
-|
-| WARNING: You MUST set this value!
-|
-| If it is not set, then CodeIgniter will try guess the protocol and path
-| your installation, but due to security concerns the hostname will be set
-| to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
-| The auto-detection mechanism exists only for convenience during
-| development and MUST NOT be used in production!
-|
-| If you need to allow multiple domains, remember that this file is still
-| a PHP script and you can easily do that on your own.
-|
-*/
-$config['base_url'] = 'http://logi.dev1.strat.is/';
+/*Load the site URL*/
+require_once(FCPATH.'application/config/main_url.php');
 
 /*
 |--------------------------------------------------------------------------
@@ -524,4 +503,4 @@ $config['rewrite_short_tags'] = FALSE;
 $config['proxy_ips'] = '';
 
 /*Loading the barcode class*/
-/*require_once(FCPATH.'barcode/barcode.class.php');*/
+require_once(FCPATH.'barcode/barcode.class.php');
