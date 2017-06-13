8f85b81999a40dcc4873656b1576aa95d3e3ca87
wwwillis <rego@wwwillis.com>
2017-06-13 18:09:12 +1000

    edit location 0.02

--- application/views/equipment/view.php
+++ application/views/equipment/view.php
@@ -1,4 +1,14 @@
 <?php $bar = new BARCODE(); ?>
+<script>
+function showEditLocation(divName) {
+    var locDiv = document.getElementById(divName);
+    locDiv.style.display = ''
+}
+function hideEditLocation(divName) {
+    var locDiv = document.getElementById(divName);
+    locDiv.style.display = 'none'
+}
+</script>
 <h2 style="text-transform: uppercase;">Equipment - <?php echo $equipment['eq_name']; ?></h2>
 <hr />
 <div class="row">
@@ -40,13 +50,13 @@
                         <td style="vertical-align: middle;"><a href="/locations/<?php echo $location['loc_id']; ?>"><?php echo $location['loc_name']; ?></a></td>
                         <td style="vertical-align: middle;"><?php echo $location['eqloc_quantity']; ?></td>
                         <td style="text-align: right;vertical-align: middle;">
-                            <a class="btn btn-basic btn-sm" role="button" href="/?/edit/<?php echo $equipment['eq_id']; ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
+                            <a class="btn btn-basic btn-sm" role="button" href="" onclick="showEditLocation('loc_view_<?php echo $location['loc_id']; ?>')"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
                             <a class="btn btn-basic btn-sm" role="button" href="/equipment/deleteLocation/<?php echo $equipment['eq_id']; ?>/<?php echo $location['eqloc_loc_id']; ?>"><i class="fa fa-times-rectangle-o" aria-hidden="true"></i> Remove</a>
                         </td>
                     </tr>
-                    <tr id="loc_edit_<?php echo $location['loc_id']; ?>" class="loc_edit" zzzstyle="display:none;">
+                    <tr id="loc_edit_<?php echo $location['loc_id']; ?>" class="loc_edit" style="display:none;">
                         <td style="vertical-align: middle;"><a href="/locations/<?php echo $location['loc_id']; ?>"><?php echo $location['loc_name']; ?></a></td>
-                        <td style="vertical-align: middle;"><form><input type="text" class="form-control" name="eqloc_quantity" value="<?php echo $location['eqloc_quantity']; ?>"><button type="submit" class="btn btn-primary btn-sm">Save</button></form></td>
+                        <td style="vertical-align: middle;"><form><input type="text" class="form-control" name="eqloc_quantity" value="<?php echo $location['eqloc_quantity']; ?>" style="width:50px;display:inline;"><button type="submit" class="btn btn-primary btn-sm">Save</button></form></td>
                         <td style="text-align: right;vertical-align: middle;">
                             <a class="btn btn-basic btn-sm" role="button" href=""><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Cancel</a>
                             <a class="btn btn-basic btn-sm disabled" role="button" href="/equipment/deleteLocation/<?php echo $equipment['eq_id']; ?>/<?php echo $location['eqloc_loc_id']; ?>"><i class="fa fa-times-rectangle-o" aria-hidden="true"></i> Remove</a>
