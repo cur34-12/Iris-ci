21b5ec368731172756582f4ec3e5386c71474824
wwwillis <rego@wwwillis.com>
2017-06-13 21:45:24 +1000

    case

--- application/models/Equipment_model.php
+++ application/models/Equipment_model.php
@@ -189,7 +189,6 @@
 			return $this->db->delete('equipment_location', array('eqloc_eq_id' => $eq_id,'eqloc_loc_id' => $loc_id));
 		}
 		public function update_equipment_location(){
-
 			$this.db.where('eqloc_loc_id',$this->input->post('eqloc_loc_id'));
 			$this.db.where('eqloc_eq_id',$this->input->post('eqloc_eq_id'));
 			return $this->db->update('equipment_location', array('eqloc_qty' => $this->input->post('eqloc_qty')));
