db27604b910d3f34c7e39e8b3fc199f172e98588
paralittic <ross.wallace@stratis-media.com>
2017-06-13 12:04:42 +1000

    Calendar test page

---
+++ application/views/pages/calendar.php
@@ -0,0 +1,14 @@
+<h2 style="text-transform: uppercase;">Calendar</h2>
+<hr />
+<div id='calendar'></div>
+<script>
+    $(document).ready(function() {
+
+        // page is now ready, initialize the calendar...
+
+        $('#calendar').fullCalendar({
+            // put your options and callbacks here
+        })
+
+    });
+</script>
