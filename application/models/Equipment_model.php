a2a3f03710eaec96dca45218fe747d5de58342b0
wwwillis <rego@wwwillis.com>
2017-06-13 22:48:16 +1000

    its gonna work

--- application/models/Equipment_model.php
+++ application/models/Equipment_model.php
@@ -191,6 +191,6 @@
 		public function update_equipment_location(){
 			$this->db->where('eqloc_loc_id',$this->input->post('eqloc_loc_id'));
 			$this->db->where('eqloc_eq_id',$this->input->post('eqloc_eq_id'));
-			return $this->db->update('equipment_location', array('eqloc_qty' => $this->input->post('eqloc_quantity')));
+			return $this->db->update('equipment_location', array('eqloc_quantity' => $this->input->post('eqloc_quantity')));
 		}		
 	}
