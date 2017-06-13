72a8e352986ed4f30bfcb10577669602a24ff818
paralittic <ross.wallace@stratis-media.com>
2017-06-13 14:26:25 +1000

    composer and local files

--- application/views/templates/footer/footer-required.php
+++ application/views/templates/footer/footer-required.php
@@ -5,20 +5,20 @@
 </div>
 
 <!-- JQuery / UI -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/components/jquery/jquery.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/components/jqueryui/jquery-ui.min.js"></script>
 
 <!--Bootstrap-->
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/components/bootstrap/js/bootstrap.js"></script>
 
 <!-- Moment Time Parser-->
-<script type="text/javascript" src="<?php echo base_url(); ?>js/moment.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/moment/moment/moment.js"></script>
 
 <!-- Chart.js -->
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/nnnick/chartjs/dist/Chart.bundle.js"></script>
 
 <!-- Full Calendar -->
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/fullcalendar/fullcalendar/dist/fullcalendar.js"></script>
 
 <!-- DataTables -->
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.15/af-2.2.0/cr-1.3.3/r-2.1.1/sc-1.4.2/se-1.2.2/datatables.min.js"></script>
