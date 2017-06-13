07470e3213414a34ff4c3c2728b789fcea9451c9
paralittic <ross.wallace@stratis-media.com>
2017-06-13 14:41:25 +1000

    more calendar testing

--- application/views/pages/calendar.php
+++ application/views/pages/calendar.php
@@ -29,7 +29,7 @@
                 {
                     id: 999,
                     title: 'Repeating Event',
-                    start: '2017-05-09T16:00:00'
+                    start: '2017-06-09T16:00:00'
                 },
                 {
                     id: 999,
@@ -38,33 +38,33 @@
                 },
                 {
                     title: 'Conference',
-                    start: '2017-05-11',
-                    end: '2017-05-13'
+                    start: '2017-06-11',
+                    end: '2017-06-13'
                 },
                 {
                     title: 'Meeting',
-                    start: '2017-05-12T10:30:00',
-                    end: '2017-05-12T12:30:00'
+                    start: '2017-06-12T10:30:00',
+                    end: '2017-06-12T12:30:00'
                 },
                 {
                     title: 'Lunch',
-                    start: '2017-05-12T12:00:00'
+                    start: '2017-06-12T12:00:00'
                 },
                 {
                     title: 'Meeting',
-                    start: '2017-05-12T14:30:00'
+                    start: '2017-06-12T14:30:00'
                 },
                 {
                     title: 'Happy Hour',
-                    start: '2017-05-12T17:30:00'
+                    start: '2017-06-12T17:30:00'
                 },
                 {
                     title: 'Dinner',
-                    start: '2017-05-12T20:00:00'
+                    start: '2017-06-12T20:00:00'
                 },
                 {
                     title: 'Birthday Party',
-                    start: '2017-05-13T07:00:00'
+                    start: '2017-06-13T07:00:00'
                 },
                 {
                     title: 'Click for Google',
