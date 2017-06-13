2989518daa4cbf0888de843b53198154fefa3fc8
wwwillis <rego@wwwillis.com>
2017-06-13 22:14:04 +1000

    baby steps

--- application/config/routes.php
+++ application/config/routes.php
@@ -34,9 +34,8 @@ $route['equipment/create'] = 'equipment/create';
 $route['equipment/update'] = 'equipment/update';
 $route['equipment/(:any)'] = 'equipment/view/$1';
 $route['equipment'] = 'equipment/index';
+$route['equipment2'] = 'equipment/index';
 $route['equipment/deletelocation/(:any)/(:any)'] = 'equipment/delete_equipment_location/$1/$2';
-$route['equipment/updatelocation'] = 'equipment/index';
-
 
 $route['equipment-groups/delete'] = 'equipment/eqgroups_select_delete';
 $route['equipment-groups/edit'] = 'equipment/eqgroups_select_edit';
