9e6d83dfa30f6a8992d01db6ce474cadf340c85b
Ross Wallace <ross.wallace@stratis-media.com>
2017-06-13 21:27:42 +1000

    hyphens

--- application/config/routes.php
+++ application/config/routes.php
@@ -34,8 +34,8 @@ $route['equipment/create'] = 'equipment/create';
 $route['equipment/update'] = 'equipment/update';
 $route['equipment/(:any)'] = 'equipment/view/$1';
 $route['equipment'] = 'equipment/index';
-$route['equipment/delete-location/(:any)/(:any)'] = 'equipment/delete_equipment_location/$1/$2';
-$route['equipment/update-location'] = 'equipment/update_equipment_location';
+$route['equipment/deletelocation/(:any)/(:any)'] = 'equipment/delete_equipment_location/$1/$2';
+$route['equipment/updatelocation'] = 'equipment/update_equipment_location';
 
 
 $route['equipment-groups/delete'] = 'equipment/eqgroups_select_delete';
