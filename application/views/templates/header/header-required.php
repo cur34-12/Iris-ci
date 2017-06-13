e05b89abcca4ca554e0014d93529e4eac1457686
paralittic <ross.wallace@stratis-media.com>
2017-06-13 15:00:26 +1000

    updating fullcalendar css

--- application/views/templates/header/header-required.php
+++ application/views/templates/header/header-required.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/v/bs/dt-1.10.15/af-2.2.0/cr-1.3.3/r-2.1.1/sc-1.4.2/se-1.2.2/datatables.min.css"/>
 
     <!-- Full Calendar Style -->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor/fullcalendar/fullcalendar/src/main.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor/fullcalendar/fullcalendar/dist/fullcalendar.css"/>
 
     <!-- JQuery / UI -->
     <script type="text/javascript" src="<?php echo base_url(); ?>vendor/components/jquery/jquery.js"></script>
