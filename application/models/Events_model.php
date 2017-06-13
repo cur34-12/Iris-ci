0574160f8266edc8245fd3efff4c56c94842b2ee
paralittic <ross.wallace@stratis-media.com>
2017-06-13 15:57:44 +1000

    event model CRU, the D might come later...giggity

--- application/models/Events_model.php
+++ application/models/Events_model.php
@@ -7,20 +7,38 @@
             $this->load->database();
         }
 
-        public function get_events($event_id = FALSE){
-            if($event_id === FALSE or is_null($event_id)){
-                $this->db->order_by('event_id', 'ASC');
+        public function get_events($event_category = FALSE){
+            if($event_category === FALSE or is_null($event_category)){
+                $this->db->order_by('$event_category', 'ASC');
                 $query = $this->db->get('events');
                 return $query->result_array();
             }
-            $query = $this->db->get_where('events', array('event_id' => $event_id));
+            $query = $this->db->get_where('events', array('$event_category' => $event_category));
             return $query->row_array();
         }
 
         public function create_event(){
             $data = array(
-                'eq_name' => $this->input->post('eq_name'),
-                'eq_group_id' => $this->input->post('eq_group_id')
+                'event_title' => $this->input->post('event_title'),
+                'event_allday' => $this->input->post('event_allday'),
+                'event_start' => $this->input->post('event_start'),
+                'event_end' => $this->input->post('event_end'),
+                'event_url' => $this->input->post('event_url'),
+                'event_classname' => $this->input->post('event_classname'),
+                'event_starteditable' => $this->input->post('event_starteditable'),
+                'event_durationeditable' => $this->input->post('event_durationeditable'),
+                'event_resourceeditable' => $this->input->post('event_resourceeditable'),
+                'event_rendering' => $this->input->post('event_rendering'),
+                'event_overlap' => $this->input->post('event_overlap'),
+                'event_constraint' => $this->input->post('event_constraint'),
+                'event_source' => $this->input->post('event_source'),
+                'event_backgroundcolor' => $this->input->post('event_backgroundcolor'),
+                'event_bordercolor' => $this->input->post('event_bordercolor'),
+                'event_textcolor' => $this->input->post('event_textcolor'),
+                'event_createdby' => $this->input->post('event_createdby'),
+                'event_createddate' => $this->input->post('event_createddate'),
+                'event_modifiedby' => $this->input->post('event_modifiedby'),
+                'event_lastmodified' => $this->input->post('event_lastmodified')
             );
 
             $this->db->insert('events', $data);
@@ -28,8 +46,27 @@
 
         public function update_events(){
             $data = array(
-                'eq_name' => $this->input->post('eq_name'),
-                'eq_group_id' => $this->input->post('eq_group_id')
+                'event_id' => $this->input->post('event_id'),
+                'event_title' => $this->input->post('event_title'),
+                'event_allday' => $this->input->post('event_allday'),
+                'event_start' => $this->input->post('event_start'),
+                'event_end' => $this->input->post('event_end'),
+                'event_url' => $this->input->post('event_url'),
+                'event_classname' => $this->input->post('event_classname'),
+                'event_starteditable' => $this->input->post('event_starteditable'),
+                'event_durationeditable' => $this->input->post('event_durationeditable'),
+                'event_resourceeditable' => $this->input->post('event_resourceeditable'),
+                'event_rendering' => $this->input->post('event_rendering'),
+                'event_overlap' => $this->input->post('event_overlap'),
+                'event_constraint' => $this->input->post('event_constraint'),
+                'event_source' => $this->input->post('event_source'),
+                'event_backgroundcolor' => $this->input->post('event_backgroundcolor'),
+                'event_bordercolor' => $this->input->post('event_bordercolor'),
+                'event_textcolor' => $this->input->post('event_textcolor'),
+                'event_createdby' => $this->input->post('event_createdby'),
+                'event_createddate' => $this->input->post('event_createddate'),
+                'event_modifiedby' => $this->input->post('event_modifiedby'),
+                'event_lastmodified' => $this->input->post('event_lastmodified')
             );
 
             $this->db->where('event_id', $this->input->post('event_id'));
