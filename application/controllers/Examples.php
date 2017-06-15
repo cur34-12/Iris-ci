910845e30d3bd57fbf5c0f8e9f2acd5bbb99f9e8
Ross Wallace <ross.wallace@stratis-media.com>
2017-06-15 18:57:01 +1000

    fixing pw reqreuiements

--- application/controllers/Examples.php
+++ application/controllers/Examples.php
@@ -193,7 +193,7 @@ class Examples extends MY_Controller
 		// Customize this array for your user
 		$user_data = [
 			'username'   => 'rwallace',
-			'passwd'     => 'welcome1234',
+			'passwd'     => 'Welcome1234!',
 			'email'      => 'ross.wallace@member.ses.nsw.gov.au',
 			'auth_level' => '9', // 9 if you want to login @ examples/index.
 		];
