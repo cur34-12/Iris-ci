9e857a2d241d2f97fcaf3dddef3db6bafbc7a3db
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-15 16:33:35 +1000

    Testing auth

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,15 +6,19 @@
 
             public function calendar()
             {
-                $data['events'] = $this->Events_model->get_events();
+                //Authentication wrapper for making sure user is a certain role
+                if( $this->require_role('admin') )
+                {
+                    $data['events'] = $this->Events_model->get_events();
 
-                $data['title'] = 'Calendar';
+                    $data['title'] = 'Calendar';
 
-                $this->load->view('templates/header/header-required', $data);
-                $this->load->view('templates/header/header-sidebar');
-                $this->load->view('templates/header/header-container');
-                $this->load->view('pages/calendar', $data);
-                $this->load->view('templates/footer/footer-required');
+                    $this->load->view('templates/header/header-required', $data);
+                    $this->load->view('templates/header/header-sidebar');
+                    $this->load->view('templates/header/header-container');
+                    $this->load->view('pages/calendar', $data);
+                    $this->load->view('templates/footer/footer-required');
+                }
             }
 
             public function view($page = 'home')
