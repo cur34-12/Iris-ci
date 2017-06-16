e6b94dcfaac0a3f3ab608bbe602d7f10adfd4e04
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-16 13:09:16 +1000

    can i use the same array twice! you be the judge

--- application/views/pages/dodash.php
+++ application/views/pages/dodash.php
@@ -37,24 +37,48 @@
         </table>
         <span style="font-size: xx-small; color: grey;">Last Updated: <?php echo $duties['duties_lastupdated']; ?>, <?php echo $duties['duties_lastupdatedby']; ?></span>
     </div>
-    <div class="col-md-8">
-        <h3 style="text-align: center;">Weather</h3>
-        <h4 style="text-transform: uppercase; letter-spacing: 1px;">Warnings</h4>
-        <table class="table table-striped table-sm">
-            <?php foreach ($rss_warnings as $warning_item) : ?>
-                <tr>
-                    <td><a target="_blank" href="<?php echo $warning_item['link']; ?>"><?php echo $warning_item['title']; ?></a></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <h4 style="text-transform: uppercase; letter-spacing: 1px;">Forcast</h4>
-        <table class="table table-striped table-sm">
-            <?php foreach ($rss_weather as $weather_item) : ?>
+    <h3 style="text-align: center;">Weather</h3>
+    <h4 style="text-transform: uppercase; letter-spacing: 1px;">Warnings</h4>
+    <table class="table table-striped table-sm">
+        <?php foreach ($rss_warnings as $warning_item) : ?>
             <tr>
-                <td><?php echo $weather_item['title']; ?></td>
-                <td><?php echo $weather_item['description']; ?></td>
+                <td><a target="_blank" href="<?php echo $warning_item['link']; ?>"><?php echo $warning_item['title']; ?></a></td>
             </tr>
-            <?php endforeach; ?>
-        </table>
+        <?php endforeach; ?>
+    </table>
+    <div class="col-md-8">
+        <div class="row">
+            <div class="col-xs-8 col-sm-6">
+                <h4 style="text-transform: uppercase; letter-spacing: 1px;">Forcast</h4>
+                <table class="table table-striped table-sm">
+                    <?php foreach ($rss_weather as $weather_item) : ?>
+                        <tr>
+                            <td><?php echo $weather_item['title'][0]; ?></td>
+                            <td><?php echo $weather_item['description'][0]; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+            <div class="col-xs-4 col-sm-6">
+                <h4 style="text-transform: uppercase; letter-spacing: 1px;">Forcast</h4>
+                <table class="table table-striped table-sm">
+                    <?php foreach ($rss_weather as $weather_item) : ?>
+                        <tr>
+                            <td><?php echo $weather_item['title'][1]; ?></td>
+                            <td><?php echo $weather_item['description'][1]; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+
+
+
+
+
+
+
+
+
+
+
     </div>
 </div>
