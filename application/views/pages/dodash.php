c2d97d24327734925c06430e23c9c6ec64a1e081
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-16 11:13:22 +1000

    dodash begin

---
+++ application/views/pages/dodash.php
@@ -0,0 +1,6 @@
+<h2 style="text-transform: uppercase; letter-spacing: 2px;">Duty Officer - Dashboard</h2>
+<div class="row">
+    <div class="col-md-4">.col-md-4</div>
+    <div class="col-md-8">.col-md-8</div>
+
+</div>
