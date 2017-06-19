a971f37ef77c372fff819ce78664ed745fdf50d8
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-19 16:09:02 +1000

    datatable pagelength

--- application/views/templates/footer/footer-required.php
+++ application/views/templates/footer/footer-required.php
@@ -35,7 +35,7 @@
                 "targets": [ -1, -2 ],
                 "orderable": false
             } ],
-            "pageLength": 25
+            "pageLength": 100
         } );
     });
 </script>
