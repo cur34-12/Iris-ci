1bf2510a1048730e195d1199a8816281a2a69d2f
Ross Wallace <ross.wallace@stratis-media.com>
2017-06-21 20:15:59 +1000

    url change

--- application/config/main_url.php
+++ application/config/main_url.php
@@ -28,4 +28,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://logi.dev1.strat.is/';
+$config['base_url'] = 'http://localhost/iris';
