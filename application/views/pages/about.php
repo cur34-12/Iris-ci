6dce591b2a0e8cd718be8b0ecc67f407160b8283
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-22 13:58:10 +1000

    more about page changes

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,10 +1,39 @@
-<h1><strong>IRIS</strong> - Unit Management System</h1>
+<h2><strong>IRIS</strong> - Unit Management System</h2>
 <hr />
-<p class="lead">
-Iris is a web application for managing unit level operations (in a non rescue context). Iris has three main focus areas:
+<h3>About IRIS</h3>
+    <p>Iris is a web application for managing unit level operations (in a non rescue context). Iris has three main focus areas:
 <ul>
     <li>Logistics Management (Inventory, Operational Capability, Equipment Maintenance and Loans)</li>
     <li>Situational Awareness (Operational Dashboards, Resource Management)</li>
     <li>Timetabling/Calendaring (Unit Resources, Leave/Holidays, Duty Rosters)</li>
 </ul>
 </p>
+
+<h3>Installing IRIS</h3>
+<p>Iris is not currently released for general usage.
+
+If you wish to use the development version of Iris, it is recommended you are familiar with the setup and management of Apache2/Nginx and MySQL. Iris is built on the Code Igniter framework and can be installed to any standard PHP capable webserver and MySQL(style) database. To use the dev version of Iris:
+
+    <ol>
+    <li>Install and configure Apache2/NGINX with PHP (if on Windows we recommend WAMP)</li>
+    <li>Install and configure a MySQL/MariaDB database server (if on Windows we recommend WAMP)</li>
+    <li>Install Composer</li>
+    <li>Clone Iris from Github into your web root</li>
+    <li>Using PhpMyAdmin or your MySQL tool or choice create an empty database called iris</li>
+    <li>Import the <code>iris-structure.sql</code> file into your new database (optional: for dummy data import the <code>iris-sample-data.sql</code> file)</li>
+    <li>Configure the <code></code>application/config/config.php</code> file to suit your environment</li>
+    <li>Configure the <code>application/config/database.php</code> file to suit your environment</li>
+    <li>Run composer update in the Iris root directory</li>
+    <li>Configure the function create_user in <code>application/controllers/Examples.php</code> to give yourself a login</li>
+    <li>Write the rest of this list!</li>
+</ol>
+</p>
+
+<h3>Contributing to IRIS</h3>
+<p>Thank you for considering contributing to the Iris! Feel free to fork the project in Github or to join the discussion, send us an email at ross.wallace@memeber.ses.nsw.gov.au.</p>
+
+<h3>Security</h3>
+<p>If you discover a security vulnerability within Iris, please send an e-mail to Ross Wallace at ross.wallace@member.ses.nsw.gov.au. All security vulnerabilities will be promptly addressed.</p>
+
+<h3>License</h3>
+<p>Iris is open-sourced software licensed under the <a href="http://opensource.org/licenses/MIT">MIT license</a>.</p>
