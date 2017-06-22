15a2b16b50895e438eccb34f298cea85c96f0a34
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-22 11:49:01 +1000

    is closed

--- application/views/templates/footer/footer.php
+++ application/views/templates/footer/footer.php
@@ -46,7 +46,7 @@
     $(document).ready(function () {
         var trigger = $('.hamburger'),
             overlay = $('.overlay'),
-            isClosed = false;
+            isClosed = true;
 
         trigger.click(function () {
             hamburger_cross();
