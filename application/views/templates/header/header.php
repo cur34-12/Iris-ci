89e2063f403c64e904b9f4fc7805682b48cfbf9f
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-22 12:22:15 +1000

    columns

--- application/views/templates/header/header.php
+++ application/views/templates/header/header.php
@@ -76,4 +76,4 @@
         </button>
         <div class="container-fluid" style="width: 95%;">
             <div class="row">
-                <div class="col-md-10">
+                <div class="col-md-12">
