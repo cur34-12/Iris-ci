7731d2c275516c9f397fa95b3a8d1382188afb62
Ross Wallace <ross.wallace@member.ses.nsw.gov.au>
2017-06-27 08:06:17 +1000

    Form fields

--- application/views/suppliers/create.php
+++ application/views/suppliers/create.php
@@ -29,62 +29,17 @@
 <div class="form-group">
     <label class="control-label col-sm-2">Do we have an account?</label>
     <div class="col-sm-10">
-        <input type="text" class="form-control" name="supplier_account" placeholder="">
+        <select id="veh_capacity" name="supplier_account" class="form-control">
+            <option>No</option>
+            <option>Yes</option>
+        </select>
+
     </div>
 </div>
 <div class="form-group">
     <label class="control-label col-sm-2">Account Reference</label>
     <div class="col-sm-10">
-        <select id="veh_capacity" name="veh_capacity" class="form-control">
-            <option>1</option>
-            <option>2</option>
-    </div>
-</div>
-<div class="form-group">
-    <label class="control-label col-sm-2">Capacity</label>
-    <div class="col-sm-10">
-        <select id="veh_capacity" name="veh_capacity" class="form-control">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-        </select>
-    </div>
-</div>
-<div class="form-group">
-    <label class="control-label col-sm-2">Category</label>
-    <div class="col-sm-10">
-        <select id="veh_category" name="veh_category" class="form-control" >
-            <option>Light Storm</option>
-            <option>Medium Storm</option>
-            <option>Heavy Storm</option>
-            <option>Light GLR</option>
-            <option>Medium GLR</option>
-            <option>Heavy GLR</option>
-            <option>Recon</option>
-            <option>Transport</option>
-        </select>
-    </div>
-</div>
-<div class="form-group">
-    <label class="control-label col-sm-2">Vehicle Type</label>
-    <div class="col-sm-10">
-        <select id="veh_type" name="veh_type" class="form-control">
-            <option>Car</option>
-            <option>Truck</option>
-            <option>Raft</option>
-            <option>Boat</option>
-            <option>Trailer</option>
-        </select>
-    </div>
-</div>
-<div class="form-group">
-    <label class="control-label col-sm-2">Location</label>
-    <div class="col-sm-10">
-        <input type="text" class="form-control" name="veh_location_id" placeholder="NOT YET IMPLEMENTED">
+        <input type="text" class="form-control" name="supplier_acount_number" placeholder="Eg. HORNSBYSES#1663">
     </div>
 </div>
 <button type="submit" class="btn btn-outline-primary">Create</button>
