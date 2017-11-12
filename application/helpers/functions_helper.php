0dabaa4816652b4f84fcb1e35cb40c4aefa0b34d
Unknown <rego@wwwillis.com>
2017-11-12 17:01:33 +1100

    arrghhh

--- application/helpers/functions_helper.php
+++ application/helpers/functions_helper.php
@@ -1,10 +1,8 @@
 <?
 //pass in dd/mm/yy or dd-mm-yy and convert to yyyy-mm-dd
-if(!function_exists('date2DB'))
-{
-  function date2DB($inputDate)
-  {
-    $dateArray = explode(substr($value,2,1),$value);
+if(!function_exists('date2DB')) {
+  function date2DB($inputDate)  {
+    $dateArray = explode(substr($inputDate,2,1),$inputDate);
     if(count($dateArray)!=3) {
       return null;
     }
