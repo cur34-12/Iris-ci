b4e8769348588e326e6059fe0c9cd887aae74d3c
Unknown <rego@wwwillis.com>
2017-11-12 17:50:50 +1100

    started off datetime2db

--- application/helpers/functions_helper.php
+++ application/helpers/functions_helper.php
@@ -1,16 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 if ( ! function_exists('date2DB'))
 {
 	/**
-	 * Site URL
+	 * date2DB
 	 *
-	 * Create a local URL based on your basepath. Segments can be passed via the
-	 * first parameter either as a string or an array.
+	 * accepts dd/mm/yy or dd-mm-yy and converts to something the DB will like. yyyy also ok
 	 *
-	 * @param	string	$uri
-	 * @param	string	$protocol
-	 * @return	string
+	 * @param	string	$inputDate
 	 */
 	function date2DB($inputDate = NULL)
 	{
@@ -23,27 +21,27 @@ if ( ! function_exists('date2DB'))
     return htmlentities($outputDate);
 	}
 }
-//pass in dd/mm/yy or dd-mm-yy and convert to yyyy-mm-dd
-/*if(!function_exists('date2DB')) {
-  function date2DB($inputDate)  {
+
+if ( ! function_exists('datetime2DB'))
+{
+	/**
+	 * date2DB
+	 *
+	 * accepts dd/mm/yy or dd-mm-yy hh:ii and converts to something the DB will like. yyyy also ok.
+	 *
+	 * @param	string	$inputDate
+	 */
+	function datetime2DB($inputDate = NULL)
+	{
     return null;
-    
-    $dateArray = explode(substr($inputDate,2,1),$inputDate);
+    /* HAVENT SET IT UP FOR TIME YET!!! */
+  /*  $dateArray = explode(substr($inputDate,2,1),$inputDate);
     if(count($dateArray)!=3) {
       return null;
     }
-    $outputDate = $dateArray[3] . "-" . $dateArray[2] . "-" . $dateArray[1];
-    return htmlentities($outputDate);
+    $outputDate = $dateArray[2] . "-" . $dateArray[1] . "-" . $dateArray[0];
+    return htmlentities($outputDate);*/
   }
 }
-/*
 
-if(!function_exists('datetime2DB'))
-{
-  function datetime2DB($value)
-  {
-   return htmlentities($value);
-  }
-}
-*/
 ?>
