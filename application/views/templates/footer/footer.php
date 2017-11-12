47cd836fbecf89172ff9513c9e8d51ff5b304600
Unknown <rego@wwwillis.com>
2017-11-12 11:38:02 +1100

    putting stuff back in footer

--- application/views/templates/footer/footer.php
+++ application/views/templates/footer/footer.php
@@ -13,13 +13,23 @@
 <!-- Bootstrap DateTime Picker-->
 <script type="text/javascript" src="<?php echo base_url(); ?>vendor/components/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js"></script>
 
+<!-- Moment Time Parser-->
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/moment/moment/moment.js"></script>
+
 <!-- D3 -->
 <script type="text/javascript" src="<?php echo base_url(); ?>vendor/mbostock/d3/d3.js"></script>
+
+<!-- Full Calendar -->
+<script type="text/javascript" src="<?php echo base_url(); ?>vendor/fullcalendar/fullcalendar/dist/fullcalendar.js"></script>
 
 <!-- DataTables -->
 <script type="text/javascript" src="<?php echo base_url(); ?>vendor/datatables/datatables/media/js/jquery.dataTables.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>vendor/datatables/datatables/media/js/dataTables.material.js"></script>
 
+<!-- Init of Moment-->
+<script>
+    moment().format();
+</script>
 
 <!-- Init of DataTable-->
 <script>
