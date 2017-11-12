b5c25db31828b2d7b3186e84ffaf655574a0f06e
Unknown <ross.wallace@stratis-media.com>
2017-11-12 13:15:33 +1100

    Adding current date/time field

--- application/views/events/create.php
+++ application/views/events/create.php
@@ -59,6 +59,12 @@
 			</select>
 		</div>
 	</div>
+	<div class="form-group">
+		<label class="control-label col-sm-2">Created DateTime</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" name="event_createddate" value="<?php echo date('Y-m-d H:i:s'); ?>">
+		</div>
+	</div>
 	<button type="submit" class="btn btn-primary">Create</button>
 	<a class="btn btn-warning" role="button" href="/events">Cancel</a>
 </form>
