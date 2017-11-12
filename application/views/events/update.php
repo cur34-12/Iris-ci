c95ac71fbdfed9632f2fb5129823079e1d369f00
Unknown <ross.wallace@stratis-media.com>
2017-11-12 12:10:54 +1100

    value capitalisation

--- application/views/events/update.php
+++ application/views/events/update.php
@@ -12,8 +12,8 @@
 		<label class="control-label col-sm-2">All Day Event</label>
 		<div class="col-sm-10">
 			<select id="event_allday" name="event_allday" class="form-control">
-				<option value="False">False</option>
-				<option value="True">True</option>
+				<option value="false">false</option>
+				<option value="true">true</option>
 			</select>
             <script>
 				$(function() {
